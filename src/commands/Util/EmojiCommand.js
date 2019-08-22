fc633fd74af3f29d1cabe0abd9eb9ba708276cf8
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2019-08-21 23:16:22 -0300

    Fazer ela detectar se o emoji é animado ou não

--- src/commands/Util/EmojiCommand.js
+++ src/commands/Util/EmojiCommand.js
@@ -15,6 +15,10 @@ module.exports = class EmojiCommand extends Command {
         
         if (!args[0]) return message.chinoReply('error', t('commands:emoji.args-null'))
 	    let emoji = Discord.Util.parseEmoji(args[0]) || message.guild.emojis.find(e => e.name === args.join(' '))
-	    message.channel.send({files: [`https://cdn.discordapp.com/emojis/${emoji.id}.png`]})
+	    if (!emoji.animated){
+		    message.channel.send({files: [`https://cdn.discordapp.com/emojis/${emoji.id}.png`]})
+	    } else {
+	    	message.channel.send({files: [`https://cdn.discordapp.com/emojis/${emoji.id}.gif`]})
+	    }
     }
 }       
