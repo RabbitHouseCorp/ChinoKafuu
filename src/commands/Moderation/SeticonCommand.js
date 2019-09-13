16aefd83e4ca264be85ad348f27ad42b3fc5e29c
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2019-09-13 16:07:43 -0300

    Update SeticonCommand.js

--- src/commands/Moderation/SeticonCommand.js
+++ src/commands/Moderation/SeticonCommand.js
@@ -12,8 +12,8 @@ module.exports = class SetIconCommand extends Command {
         })
     } 
     execute({message, args, server}, t) {
-        let icon = args[0] || message.attachments.first().url
-        if (!icon) return message.chinoReply('error', t('commands:seticon.args-null'))
+        let icon = args[0] || message.attachments.first() ? message.attachments.first().url : undefined
+        if (!icon || icon === undefined) return message.chinoReply('error', t('commands:seticon.args-null'))
 
         message.guild.setIcon(icon).then(() => {
             message.chinoReply('success', t('commands:seticon.success'))
