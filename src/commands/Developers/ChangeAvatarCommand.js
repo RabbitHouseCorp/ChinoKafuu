a3f21c16d7173f3f4fbfd4dd17d63c8dce5e7b29
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2019-09-13 16:02:22 -0300

    Fix ChangeAvatarCommand.js

--- src/commands/Developers/ChangeAvatarCommand.js
+++ src/commands/Developers/ChangeAvatarCommand.js
@@ -13,8 +13,8 @@ module.exports = class ChangeAvatarCommand extends Command {
    } 
    execute({message, args, server}, t) {
         
-        const avatar = args[0] || message.attachments.first().url
-        if (!avatar) return message.chinoReply('error', t('commands:changeavatar.args-null'))
+        const avatar = args[0] || message.attachments.first() ? message.attachments.first().url : undefined
+        if (!avatar || avatar === undefined) return message.chinoReply('error', t('commands:changeavatar.args-null'))
 
         this.client.user.setAvatar(avatar).then(() => {
             const embed = new this.client.Discord.RichEmbed()
@@ -23,6 +23,6 @@ module.exports = class ChangeAvatarCommand extends Command {
             .setImage(avatar)
 
             message.channel.send(embed)
-        }) 
+        })
     }
 }
