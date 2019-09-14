5a08c26650516a608e17ecdd02d8b0b8cb7a1d32
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2019-09-14 01:21:08 -0300

    Update YenCommand.js

--- src/commands/Economy/YenCommand.js
+++ src/commands/Economy/YenCommand.js
@@ -9,16 +9,18 @@ module.exports = class YensCommand extends Command {
         })
     }
     async execute({message, args, server}, t) {
-        let user = await this.client.database.Users.findById(message.author.id)
+        let member = message.mentions.users.first() || this.client.users.get(args[0]) || message.author
+        let user = await this.client.database.Users.findById(member.id)
         if (!user || user === null) {
             let novoUser = new this.client.database.Users({
-                '_id': message.author.id
+                '_id': member.id
             })
             novoUser.save()
         }
-
-        message.chinoReply('yen', t('commands:yen.totalYens', {
-          yens: user.yens
-        }))
+        if (message.author.id === member.id) {
+            message.chinoReply('yen', t('commands:yen.totalYens', {yens: user.yens))
+        } else {
+            message.chinoReply("yen", t("commands:yen.user-total", {member: member, yens: user.yens}))
+        }
     }
 }
