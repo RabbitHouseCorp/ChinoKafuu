d22b8cccb19f4acd8ecef26a2119ea8bc4311dbc
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2019-09-15 12:58:41 -0300

    Vamos ver se dessa vez funciona

--- src/commands/Moderation/AddEmojiCommand.js
+++ src/commands/Moderation/AddEmojiCommand.js
@@ -10,17 +10,24 @@ module.exports = class AddEmoji extends Command {
            OnlyDevs: false,
            hidden: false,
        })
-   } 
+   }
    execute({message, args, server}, t) {
             
         const url = message.attachments.first() ? message.attachments.first().url : undefined || args[1]
         if (!url || url === undefined) return message.chinoReply("error", t("commands:addemoji.args-null"))
         const name = args[0]
-
-        message.guild.createEmoji(url, name).then(emoji => {
-            message.channel.send(`${emoji} **|** ${message.author}, ${t('commands:addemoji.success')}`)
-        }).catch(() => {
-            message.chinoReply("error", t('commands:addemoji.error'))
-        })
+        if (!message.attachments.firt()) {
+            message.guild.createEmoji(url, name).then(emoji => {
+                message.channel.send(`${emoji} **|** ${message.author}, ${t('commands:addemoji.success')}`)
+            }).catch(() => {
+                message.chinoReply("error", t('commands:addemoji.error'))
+            })
+        } else {
+            message.guild.createEmoji(url, name).then(emoji => {
+                message.channel.send(`${emoji} **|** ${message.author}, ${t('commands:addemoji.success')}`)
+            }).catch(() => {
+                message.chinoReply("error", t('commands:addemoji.error'))
+            })
+        }
     }
 }
