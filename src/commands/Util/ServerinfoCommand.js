7856e523debddbd2b9c58981d2bec1087d29713d
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2019-09-29 13:12:08 -0300

    Whoops, fiz caquinha aqui

--- src/commands/Util/ServerinfoCommand.js
+++ src/commands/Util/ServerinfoCommand.js
@@ -24,24 +24,24 @@ module.exports = class ServerinfoCommand extends Command {
         let roles = `${message.guild.roles.map(r => `\`${r.name}\``).join(', ')}`.replace('`@everyone`,', '')
         
         const embed = new this.client.Discord.RichEmbed()
-        .setAuthor(t('commands:serverinfo.name', {name: message.guild.name}), message.guild.iconURL)
-        .setColor(this.client.colors.default)
+        embed.setAuthor(t('commands:serverinfo.name', {name: message.guild.name}), message.guild.iconURL)
+        embed.setColor(this.client.colors.default)
         message.guild.icon ? embed.setThumbnail(`${message.guild.iconURL}`.replace('jpg', 'png')) : null
-        .addField(t('commands:serverinfo.guildName'), message.guild.name, true)
-        .addField(t('commands:serverinfo.guildID'), message.guild.id, true)
-        .addField(t('commands:serverinfo.guildOwnerTag'), message.guild.owner.user.tag, true)
-        .addField(t('commands:serverinfo.guildOwnerID'), message.guild.owner.user.id, true)
-        .addField(t('commands:serverinfo.guildRegion'), message.guild.region, true)
-        .addField(t('commands:serverinfo.afkChannel'), `${message.guild.afkChannel}`.replace('null', t('commands:serverinfo.noAfkChannel')), true)
-        .addField(t('commands:serverinfo.createdAt'), moment.utc(message.guild.createdAt).format('LLLL'), true)
+        embed.addField(t('commands:serverinfo.guildName'), message.guild.name, true)
+        embed.addField(t('commands:serverinfo.guildID'), message.guild.id, true)
+        embed.addField(t('commands:serverinfo.guildOwnerTag'), message.guild.owner.user.tag, true)
+        embed.addField(t('commands:serverinfo.guildOwnerID'), message.guild.owner.user.id, true)
+        embed.addField(t('commands:serverinfo.guildRegion'), message.guild.region, true)
+        embed.addField(t('commands:serverinfo.afkChannel'), `${message.guild.afkChannel}`.replace('null', t('commands:serverinfo.noAfkChannel')), true)
+        embed.addField(t('commands:serverinfo.createdAt'), moment.utc(message.guild.createdAt).format('LLLL'), true)
     
         const page2 = new this.client.Discord.RichEmbed()
-        .setAuthor(t('commands:serverinfo.name', {name: message.guild.name}), message.guild.iconURL)
-        .setColor(this.client.colors.default)
+        embed.setAuthor(t('commands:serverinfo.name', {name: message.guild.name}), message.guild.iconURL)
+        embed.setColor(this.client.colors.default)
         message.guild.icon ? embed.setThumbnail(`${message.guild.iconURL}`.replace('jpg', 'png')) : null
-        .addField(t('commands:serverinfo.guildChannel', {totalChannel: message.guild.channels.size}), t('commands:serverinfo.channels', {voice: voice, text: text, category: category}), true)
-        .addField(t('commands:serverinfo.guildMembers', {totalMember: message.guild.memberCount}), t('commands:serverinfo.members', {user: user, bot: bot}), true)
-        .addField(t('commands:serverinfo.roles', {totalRole: message.guild.roles.size - 1}), roles, true)
+        page2.addField(t('commands:serverinfo.guildChannel', {totalChannel: message.guild.channels.size}), t('commands:serverinfo.channels', {voice: voice, text: text, category: category}), true)
+        page2.addField(t('commands:serverinfo.guildMembers', {totalMember: message.guild.memberCount}), t('commands:serverinfo.members', {user: user, bot: bot}), true)
+        page2.addField(t('commands:serverinfo.roles', {totalRole: message.guild.roles.size - 1}), roles, true)
         
         message.channel.send(embed).then(msg => {
             setTimeout(function() {
@@ -52,6 +52,7 @@ module.exports = class ServerinfoCommand extends Command {
             }, 1000)
             const collector = msg.createReactionCollector((r, u) => (r.emoji.name === '⬅', '➡') && (u.id !== this.client.user.id && u.id === message.author.id))
             collector.on('collect', r => {
+                r.remove(message.author.id)
                 switch (r.emoji.name) {
                     case '⬅':
                         msg.edit(embed)
