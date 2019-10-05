c1fb5682c77b421e34be98f9b2dceb2e2858b3ad
DanielaGC <danielagc676@gmail.com>
2019-10-05 15:50:27 -0300

    Fix SayCommand

--- src/commands/Random/SayCommand.js
+++ src/commands/Random/SayCommand.js
@@ -15,7 +15,7 @@ module.exports = class SayCommand extends Command {
         
     let say = args.join(' ');
     if (!args.join(" ")) return message.chinoReply('error', t('commands:say.args-null'));
-    if (message.member.hasPermission("MENTION_EVERYONE")) {
+    if (!message.member.hasPermission("MENTION_EVERYONE")) {
       if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.createWebhook(message.author.username, message.author.displayAvatarURL).then(w => {
         w.send(args.join(' '), {
           disableEveryone: false
