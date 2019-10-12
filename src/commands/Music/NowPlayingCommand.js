25061895aed78bf28cf2105518e473d857b613f2
DanielaGC <danielagc676@gmail.com>
2019-10-12 15:19:06 -0300

    Fixed NowPlayingCommand

--- src/commands/Music/NowPlayingCommand.js
+++ src/commands/Music/NowPlayingCommand.js
@@ -1,7 +1,6 @@
 const Command = require("../../structures/command")
 const { RichEmbed } = require("discord.js")
 const moment = require("moment")
-let youtube = require("youtube-info")
 require("moment-duration-format")
 module.exports = class NowPlayingCommand extends Command {
     constructor(client) {
@@ -23,22 +22,21 @@ module.exports = class NowPlayingCommand extends Command {
         if (!message.member.voiceChannel) return message.chinoReply('error', t('commands:dj-module.channel-null'))
         if (message.guild.me.voiceChannel && message.member.voiceChannel !== message.guild.me.voiceChannel) return message.chinoReply('error', t('commands:dj-module.another-channel'))
         message.channel.send(t("commands:np.waiting")).then(msg => {
-            youtube(this.client.player.get(message.guild.id).nowPlaying.identifier).then(info => {
-                let start = moment.duration(this.client.player.get(message.guild.id).player.state.position).format("dd:hh:mm:ss")
-                let end = moment.duration(this.client.player.get(message.guild.id).nowPlaying.length).format("dd:hh:mm:ss")
-                let volume = `${this.client.player.get(message.guild.id).player.state.volume}/150`
-                const embed = new RichEmbed()
-                .setColor(this.client.colors.default)
-                .setURL(info.url)
-                .setThumbnail(info.thumbnailUrl)
-                .setTitle(t("commands:np.nowplaying"))
-                .addField(t("commands:np.name"), info.title)
-                .addField(t("commands:np.time"), `${start}/${end}`)
-                .addField(t("commands:np.volume"), volume)
-                .addField(t("commands:np.url"), info.url)
+            let start = moment.duration(this.client.player.get(message.guild.id).player.state.position).format("dd:hh:mm:ss")
+            let end = moment.duration(this.client.player.get(message.guild.id).nowPlaying.length).format("dd:hh:mm:ss")
+            let volume = `${this.client.player.get(message.guild.id).player.state.volume}/150`
+            let player = this.client.player.get(message.guild.id).nowPlaying
+            const embed = new RichEmbed()
+            .setColor(this.client.colors.default)
+            .setURL(player.uri)
+            .setThumbnail("https://images-ext-2.discordapp.net/external/VIv5AECItz2CCjAv3paYWWu8jTzpjqMmSzUovOaq3lM/https/developers.soundcloud.com/assets/powered_by_large_white-9c2af6a93ad2b1c541f423d9e9045980.png")
+            .setTitle(t("commands:np.nowplaying"))
+            .addField(t("commands:np.name"), player.title)
+            .addField(t("commands:np.time"), `${start}/${end}`)
+            .addField(t("commands:np.volume"), volume)
+            .addField(t("commands:np.url"), player.uri)
 
-                msg.edit(embed)
-            })
+            msg.edit(embed)
         })
     }
 }
