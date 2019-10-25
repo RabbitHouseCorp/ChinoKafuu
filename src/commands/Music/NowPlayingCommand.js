f4cb6329cfee585ed877e4ad1791bc5f345205c6
DanielaGC <danielagc676@gmail.com>
2019-10-25 15:01:22 -0300

    Fixed UserInfoCommand

--- src/commands/Music/NowPlayingCommand.js
+++ src/commands/Music/NowPlayingCommand.js
@@ -1,6 +1,7 @@
 const Command = require("../../structures/command")
 const { RichEmbed } = require("discord.js")
 const moment = require("moment")
+let youtube = require("youtube-info")
 require("moment-duration-format")
 module.exports = class NowPlayingCommand extends Command {
     constructor(client) {
@@ -15,12 +16,14 @@ module.exports = class NowPlayingCommand extends Command {
         })
     }
 
-    run({message, args, server}, t) {
+    async run({message, args, server}, t) {
 
         if (!this.client.player.has(message.guild.id)) return message.chinoReply("error", t("commands:dj-module.queue-null"))
         if (!this.client.player.get(message.guild.id).nowPlaying) return message.chinoReply("error", t("commands:dj-module.queue-null"))
         if (!message.member.voiceChannel) return message.chinoReply('error', t('commands:dj-module.channel-null'))
         if (message.guild.me.voiceChannel && message.member.voiceChannel !== message.guild.me.voiceChannel) return message.chinoReply('error', t('commands:dj-module.another-channel'))
+        let yt = await youtube(this.client.player.get(message.guild.id).nowPlaying.identifier)
+
         message.channel.send(t("commands:np.waiting")).then(msg => {
             let start = moment.duration(this.client.player.get(message.guild.id).player.state.position).format("dd:hh:mm:ss")
             let end = moment.duration(this.client.player.get(message.guild.id).nowPlaying.length).format("dd:hh:mm:ss")
@@ -29,7 +32,7 @@ module.exports = class NowPlayingCommand extends Command {
             const embed = new RichEmbed()
             .setColor(this.client.colors.default)
             .setURL(player.uri)
-            .setThumbnail("https://images-ext-2.discordapp.net/external/VIv5AECItz2CCjAv3paYWWu8jTzpjqMmSzUovOaq3lM/https/developers.soundcloud.com/assets/powered_by_large_white-9c2af6a93ad2b1c541f423d9e9045980.png")
+            .setThumbnail(yt.thumbnailUrl)
             .setTitle(t("commands:np.nowplaying"))
             .addField(t("commands:np.name"), player.title)
             .addField(t("commands:np.time"), `${start}/${end}`)
