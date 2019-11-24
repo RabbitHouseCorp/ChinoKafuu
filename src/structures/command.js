1c46c85dde49e33072dcbcb3cde2de15c5672193
Mystery <pedropivapedropiva1@outlook.com>
2019-11-24 00:32:05 -0300

    pai nosso que estais no ceu louvado seja o senhor

--- src/structures/command.js
+++ src/structures/command.js
@@ -1,42 +1,23 @@
 module.exports = class Command {
-  constructor(client, options) {
-    this.client = client;
+    constructor(client, options) {
+        this.client = client;
 
-    this.config = {
-      name: options.name || null,
-      category: options.category || "util",
-      aliases: options.aliases || [],
-      UserPermission: options.UserPermission || null,
-      ClientPermission: options.ClientPermission || null,
-      OnlyDevs: options.OnlyDevs || false,
-      hidden: false
+        this.config = {
+            name: options.name || null,
+            category: options.category || "util",
+            aliases: options.aliases || [],
+            UserPermission: options.UserPermission || null,
+            ClientPermission: options.ClientPermission || null,
+            OnlyDevs: options.OnlyDevs || false,
+            hidden: false
+        }
     }
-  }
 
-  setT (t) {
-    this.config.t = t
-  }
+    setT (t) {
+        this.config.t = t
+    }
   
-  getT() {
-    return this.config.t
-  }
-
-  getOption (message, yes = ['adicionar', 'adc', 'add', 'insert'], no = ['remover', 'remove', 'delete', 'deletar']) {
-    const cleanMessage = string.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
-    if (yes.filter(a => a === cleanMessage)[0]) return 'yes'
-    if (no.filter(a => a === cleanMessage)[0]) return 'no'
-    return null
-  }
-  
-  getEmoji (message, text) {
-    return message.guild.emojis.get((text.split(':')[2] || '').replace('>', '')) || message.guild.roles.get(text) || message.guild.emojis.find(a => a.name.toLowerCase() === text.toLowerCase()).first()
-  }
-  getRole (message, text, useMention = false) {
-    if (message.mentions.roles.first() && text && useMention) return message.mentions.roles.first()
-    else return message.guild.roles.get(text) || message.guild.roles.get(text.split('@&')[1].replace('>', '')) || message.guild.roles.find(a => a.name.toLowerCase() === text.toLowerCase()).first()
-  }
-  getUser (message, text, useMention = false) {
-    if (message.mentions.users.first() && text && useMention) return message.mentions.users.first()
-    else return message.guild.users.get(text) || message.guild.users.get(text.split('@')[1].replace('>', '')) || message.guild.users.find(a => a.username.toLowerCase() === text.toLowerCase()).first()
-  }
+    getT() {
+        return this.config.t
+    }
 }
