c46bbdac8487015edbe4db5578a6bd0c65fa0dea
DanielaGC <danielagc676@gmail.com>
2019-12-01 22:01:43 -0300

    Fixed remove and resume command

--- src/commands/Music/RemoveCommand.js
+++ src/commands/Music/RemoveCommand.js
@@ -17,8 +17,7 @@ module.exports = class RemoveCommand extends Command {
         let number = args[0] < 0 || args[0] > this.client.player.get(message.guild.id).queue.length
         if (number) return message.chinoReply("error", t("commands:remove.max-and-minimum", {max: this.client.player.get(message.guild.id).queue.length}))
 
-        message.chinoReply("sucess", t("commands:remove.removed")).then(() => {
-            this.client.player.get(message.guild.id).queue.splice(Number(args[0]) - 1, 1)  
-        })
+        message.chinoReply("sucess", t("commands:remove.removed"))
+        this.client.player.get(message.guild.id).queue.splice(Number(args[0]) - 1, 1)
     }
 }
