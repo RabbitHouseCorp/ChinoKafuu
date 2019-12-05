57d1e271bfdb0ba6018954fcba39466e15657d67
Mystery <pedropivapedropiva1@outlook.com>
2019-12-05 03:21:04 -0300

    dog days are ooover

---
+++ src/commands/Fun/RollCommand.js
@@ -0,0 +1,18 @@
+const Command = require("../../structures/command.js")
+
+class RollCommand extends Command {
+  constructor(client) {
+    super(clent, {
+      name: "roll",
+      category: "fun",
+      aliases: ["rolldie', 'die"],
+      UserPermission: null,
+      ClientPermission: null
+    })
+  }
+  async run ({message, args}, t) {
+    let numbers = ["1, 2, 3, 4, 5, 6"]
+    let result = number[Math.floor(Math.random()*numbers.length)]
+    message.channel.send("🎲 | " + result)
+  }
+}
