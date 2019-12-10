e6a6bd571a4e4dfab816141a3b9280f41a72e463
Paulo <paulosouza.ti@gmail.com>
2019-12-09 23:55:12 -0300

    money command

---
+++ src/commands/Util/MoneyCommand.js
@@ -0,0 +1,52 @@
+const Command = require("../../structures/command");
+const ExchangeApi = require('../../api/exchangeApi');
+const InvalidArgumentError = require('../../error/invalidArgumentError');
+
+class LeaveCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'money',
+      category: 'util',
+      aliases: ['bufunfa', 'currency', 'convercao'],
+      UserPermission: [],
+      ClientPermission: null,
+      OnlyDevs: false,
+      hidden: false,
+    })
+  }
+  async run({ message, args, server }, t) {
+    let [ from, to, ammount = 1 ] = args;
+
+    const { moneybag } = this.client.emotes;
+
+    if (!from || !to || (ammount === null)) {
+      message.chinoReply('error', t('commands:money.invalidOptions', {
+        prefix: server.prefix,
+        name: this.config.name,
+      }));
+
+      return;
+    }
+
+    if (isNaN(ammount)) {
+      message.chinoReply('error',t('command:money.invalidValue'))
+      return;
+    }
+
+    from = from.toUpperCase();
+    to = to.toUpperCase();
+
+    try {
+      const res = await ExchangeApi.getInstance().getCurrency(from.toUpperCase(), to.toUpperCase());
+      const total = (res.rates[to] * parseFloat(ammount)).toFixed(2);
+
+      message.chinoReply('success', moneybag + ' ' + t('commands:money.success', { from, to, ammount, total }));
+    } catch (err) {
+      if (err instanceof InvalidArgumentError) {
+        message.chinoReply('error',t('commands:money.invalidArgument', { arg: err.message }));
+      }
+    }
+  }
+}
+
+module.exports = LeaveCommand;
