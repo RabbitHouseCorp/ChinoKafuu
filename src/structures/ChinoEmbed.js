90cccf8686eea2a52aa6612b5cf380afcccd23e5
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-01-02 23:14:45 -0300

    Delete ChinoEmbed.js

--- src/structures/ChinoEmbed.js
+++
@@ -1,9 +0,0 @@
-const { RichEmbed } = require('discord.js');
-
-module.exports = class ChinoEmbed extends RichEmbed {
-    constructor(user, data = {}) {
-        super(data)
-        this.setColor('#6b8dff')
-        if (user) this.setFooter(user.tag, user.avatarURL).setTimestamp()
-    }
-}
