6b2477b3432ae69db21ef943902d44a01890b947
Pablo Vinícius <43940190+AvengerSuicide@users.noreply.github.com>
2020-02-13 15:20:14 -0300

    status.json created for the purpose of optimizing 'ready' listener

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,38 +1,41 @@
-const lavalinkManager = require('../lavalink/lavalinkManager')
-const DBL = require("dblapi.js")
+const lavalinkManager = require('../lavalink/lavalinkManager');
+const DBL = require('dblapi.js');
+
+const STATUS = require('../structures/status.json').status;
+
 module.exports = class {
   constructor(client) {
-      this.client = client;
+    this.client = client;
   }
 
-  async run () {
-
-    const dbl = new DBL(this.client.config.dbltoken, this.client)
-    dbl.on("posted", () => {
-      console.log("Connected to DBL")
-    })
-    console.log(`${this.client.user.username} has be connected to Discord`)
-    this.client.owner = await this.client.fetchUser('395788326835322882')
-    this.client.lavalinkManager = new lavalinkManager(this.client)
-    let status = [
-        {name: `se precisar de suporte, use ${this.client.config.prefix}ajuda`, type: 'PLAYING'},
-        {name: `descubra os meus comandos usando ${this.client.config.prefix}comandos`, type: "PLAYING"},
-        {name: `com a Cocoa Hoto no Rabbit House 🐰`, type: "WATCHING"},
-        {name: `🐦 me siga no twitter: @ChinoKafuuBot`, type: 'PLAYING'},
-        {name: `eu fui criada por ${this.client.owner.tag}`, type: 'PLAYING'},
-        {name: `me adicione usando ${this.client.config.prefix}convite`, type: 'LISTENING'},
-        {name: `alegria para ${Number(this.client.users.size).toLocaleString()} usuários!`, type: 'STREAMING', url: 'https://www.twitch.tv/danielagc'},
-        {name: `entre em meu servidor de suporte usando ${this.client.config.prefix}ajuda`, type: 'PLAYING'},
-        {name: 'Gochuumon Wa Usagi Desu Ka?', type: 'WATCHING'},
-        {name: `se encontrou algum bug, use ${this.client.config.prefix}ajuda e avise a minha equipe de desenvolvimento.`, type: "STREAMING", url: "https://www.twitch.tv/danielagc"},
-        {name: 'coelhos são fofos, ninguém discorda disso! 🐰', type: 'STREAMING', url: 'https://twitch.tv/danielagc'},
-        {name: `muito amor para ${Number(this.client.users.size).toLocaleString()} usuários`, type: 'STREAMING', url: 'https://twitch.tv/danielagc'},
-        {name: `café é a minha bebida favorita.`, type: 'PLAYING'}
-    ];
-      
-    setInterval(() => {
-      let randomStatus = status[Math.floor(Math.random() * status.length)]
-      this.client.user.setPresence({game: randomStatus})
-    }, 30000)
+  replaceStatus(status = 'café é a minha bebida favorita.') {
+    const { guilds, users, config } = this.client;
+    return status
+      .replace(/{users}/gi, users.size)
+      .replace(/{guilds}/gi, guilds.size)
+      .replace(/{prefix}/gi, config.prefix);
   }
-}
+
+  async run() {
+    console.log(`${this.client.user.username} has be connected to Discord`);
+
+    const dbl = new DBL(this.client.config.dbltoken, this.client);
+    dbl.on('posted', () => console.log('Connected to DBL'));
+
+    this.client.owner = await this.client.fetchUser('395788326835322882');
+    this.client.lavalinkManager = new lavalinkManager(this.client);
+
+    const changePresence = () => {
+      const presence = STATUS.sort(() => (Math.random() > 0.5 ? -1 : 1))[0];
+      this.client.user.setPresence({
+        game: {
+          ...presence,
+          name: this.replaceStatus(presence.name)
+        }
+      });
+    };
+
+    changePresence();
+    setInterval(() => changePresence, 30000);
+  }
+};
