d7e7f3411efc4b5bc05f561da4d7b01810e71cfb
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-04-19 23:05:38 -0300

    Update DailyCommand.js

--- src/commands/economy/DailyCommand.js
+++ src/commands/economy/DailyCommand.js
@@ -1,5 +1,7 @@
 const Command = require("../../structures/command")
 const moment = require("moment")
+const { MessageEmbed } = require ("discord.js")
+const DBL = require("dblapi.js")
 module.exports = class DailyCommand extends Command {
 	constructor(client) {
 		super(client, {
@@ -10,6 +12,7 @@ module.exports = class DailyCommand extends Command {
 	}
 	async run({ message, args, server }, t) {
 		let user = await this.client.database.Users.findById(message.author.id)
+		const dbl = new DBL(this.client.config.dbltoken, this.client)
 		if (!user || user === null) {
 			new this.client.database.Users({
 				_id: message.author.id
@@ -17,6 +20,16 @@ module.exports = class DailyCommand extends Command {
 		}
 
 		if (parseInt(user.timeDaily) < Date.now()) {
+
+			let checkVote = await dbl.hasVoted(message.author.id)
+			const embed = new MessageEmbed()
+			.setColor(this.client.colors.default)
+			.setAuthor("Estamos quase lá!", message.author.displayAvatarURL({ format: "png" }))
+			.setThumbnail("https://cdn.discordapp.com/attachments/484897060093689857/701374698390421534/685197956466344016.gif")
+			.setDescription("Para você conseguir pegar o seus yens, você precisará votar em mim para poder liberar, a cada 12 horas, você poderá pegar os seus yens.")
+			.addField("Deseja continuar?", "Se você estiver de acordo e deseja continuar, então clique [aqui](https://top.gg/bot/481282441294905344/vote), logo após votar, você precisará executar o comando novamente.")
+			
+			if (!checkVote) return message.channel.send(embed)
 
 			let random = Math.floor(Math.random() * (1400 - 340 + 1)) + 340
 
