4ee26dd773f8fb91279a9e863ad9c43f07a64bef
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-05-30 18:41:25 -0300

    Fixed ChannelinfoCommand²

--- src/commands/util/ChannelinfoCommand.js
+++ src/commands/util/ChannelinfoCommand.js
@@ -22,7 +22,7 @@ module.exports = class ChannelInfoCommand extends Command {
 			.addField(t("commands:channelinfo.id"), `\`${channel.id}\``, true)
 			.addField(t("commands:channelinfo.nsfw"), channel.nsfw ? t("commands:channelinfo.true") : t("commands:channelinfo.false"), true)
 			.addField(t("commands:channelinfo.guild"), `\`${channel.guild.name}\``, true)
-			.addField(t("commands:channelinfo.category"), channel.parent.name, true)
+			.addField(t("commands:channelinfo.category"), channel.parent ? channel.parent.name : "This channel is not in any category.", true)
 			.addField(t("commands:channelinfo.created-at"), moment.utc(channel.createdAt).format("LLLL"), true)
 
 		message.channel.send(embed)
