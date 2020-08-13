83218cc0f2c894228bd9f344d8696a0bcc138ae9
DanielaGC <danielagc676@gmail.com>
2020-08-13 20:09:51 -0300

    Fixed mentions

--- src/commands/mod/AnnounceCommand.js
+++ src/commands/mod/AnnounceCommand.js
@@ -52,12 +52,12 @@ module.exports = class AnnounceCommand extends Command {
 			collector.on("collect", r => {
 				switch (r.emoji.name) {
 					case "success":
-						chat.send("@everyone", { embed: embed, allowedMentions: ["everyone"] })
+						chat.send("@everyone", { embed: embed, allowedMentions: { everyone: true } })
 						msg.delete()
 						message.chinoReply("success", t("commands:announce.send"))
 						break
 					case "warn":
-						chat.send("@here", { embed: embed, allowedMentions: ["everyone"] })
+						chat.send("@here", { embed: embed, allowedMentions: { everyone: true } })
 						msg.delete()
 						message.chinoReply("success", t("commands:announce.send"))
 						break
