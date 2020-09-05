04a332b5ea843cf2898d100f5cd621ec58789a0d
DanielaGC <danielagc676@gmail.com>
2020-09-05 01:02:56 -0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,4 @@
-const lavalinkManager = require("../lavalink/lavalinkManager")
+// const lavalinkManager = require("../lavalink/lavalinkManager")
 const DBL = require("dblapi.js")
 const Sentry = require('@sentry/node')
 module.exports = class {
@@ -8,12 +8,12 @@ module.exports = class {
 
 	async run() {
 		const dbl = new DBL(this.client.config.dbltoken, this.client)
-		let guilds = await this.client.shardManager.getAllSizeObject("guilds")
-		let users = await this.client.shardManager.getAllSizeObject("users")
+		// let guilds = await this.client.shardManager.getAllSizeObject("guilds")
+		// let users = await this.client.shardManager.getAllSizeObject("users")
 		dbl.postStats(this.client.guilds.cache.size, this.client.shard.ids, this.client.shard.count)
 		console.log(`${this.client.user.username} has been connected to Discord`)
 		this.client.owner = await this.client.users.fetch("395788326835322882")
-		this.client.lavalinkManager = new lavalinkManager(this.client)
+		// this.client.lavalinkManager = new lavalinkManager(this.client)
 		let status = [
 			{ name: `If you need support, use ${this.client.config.prefix}help`, type: "PLAYING" },
 			{ name: `Find out it my commands using ${this.client.config.prefix}commands`, type: "PLAYING" },
@@ -21,14 +21,14 @@ module.exports = class {
 			{ name: "🐦 Follow me in twitter: @ChinoKafuuBot", type: "PLAYING" },
 			{ name: `I was created by ${this.client.owner.tag}`, type: "PLAYING" },
 			{ name: `Add me using ${this.client.config.prefix}invite`, type: "LISTENING" },
-			{ name: `Happiless for ${Number(users).toLocaleString()} users!`, type: "WATCHING" },
+			// { name: `Happiless for ${Number(users).toLocaleString()} users!`, type: "WATCHING" },
 			{ name: `Join in my support server using ${this.client.config.prefix}help or ${this.client.config.prefix}invite`, type: "PLAYING" },
 			{ name: "Gochuumon Wa Usagi Desu Ka?", type: "WATCHING" },
 			{ name: `If you find a bug, use ${this.client.config.prefix}help and warn my developer team.`, type: "PLAYING" },
 			{ name: "Rabbits are cute, and nobody discord this! 🐰", type: "LISTENING" },
-			{ name: `Much love for ${Number(users).toLocaleString()} users`, type: "PLAYING", url: "https://twitch.tv/danielagc" },
+			// { name: `Much love for ${Number(users).toLocaleString()} users`, type: "PLAYING", url: "https://twitch.tv/danielagc" },
 			{ name: "Coffee is my favourite drink.", type: "PLAYING" },
-			{ name: `${Number(guilds).toLocaleString()} guilds`, type: "WATCHING" }
+			// { name: `${Number(guilds).toLocaleString()} guilds`, type: "WATCHING" }
 		]
 
 		setInterval(() => {
