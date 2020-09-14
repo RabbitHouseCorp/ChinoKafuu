e27e24498fb91e6c31757992433374d72b00169a
Blue Slime <33792776+BlueSlimee@users.noreply.github.com>
2020-09-14 18:19:15 -0400

    Fix security issue

--- src/commands/util/CalculateCommand.js
+++ src/commands/util/CalculateCommand.js
@@ -1,5 +1,25 @@
 const Command = require("../../structures/command")
-let math = require("mathjs")
+const workerpool = require('workerpool');
+const pool = workerpool.pool();
+
+function calc (expr) {
+	const { create, all } = require('mathjs')
+	const math = create(all)
+	const limitedEvaluate = math.evaluate
+
+	math.import({
+		'import': 'Function import is disabled',
+		'createUnit': 'Function createUnit is disabled',
+		'evaluate': 'Function evaluate is disabled',
+		'parse': 'Function parse is disabled',
+		'simplify': 'Function simplify is disabled',
+		'derivative': 'Function derivative is disabled',
+		'format': 'Function format is disabled'
+	}, {override: true})
+
+	return limitedEvaluate(expr).toString()
+}
+
 module.exports = class CalculateCommand extends Command {
 	constructor(client) {
 		super(client, {
@@ -15,12 +35,9 @@ module.exports = class CalculateCommand extends Command {
 
 		let question = args.join(" ")
 		if (!question) return message.chinoReply("error", t("commands:calc.args-null"))
-		function calc(expression) {
-			'use strict';
-			return math.evaluate(expression)
-		}
-		let resposta = calc(question)
 
-		message.chinoReply("diamond", t("commands:calc.result", { resposta: resposta }))
+		pool.exec(calc, [question]).then((r) => message.chinoReply("diamond", t("commands:calc.result", { resposta: r })))
+			.catch((e) => message.chinoReply("error", t("commands:calc.result", { resposta: e.stack })))
+			.then(() => pool.terminate())
 	}
 }
