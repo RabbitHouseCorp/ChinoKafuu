0f2062455566bfbf0ba50ab476283b6dd7c7acd6
mystery <38635037+MysteryMS@users.noreply.github.com>
2020-10-24 02:14:48 -0300

    ping bonito

--- src/commands/misc/PingCommand.js
+++ src/commands/misc/PingCommand.js
@@ -10,24 +10,22 @@ class PingCommand extends Command {
   async run(ctx) {
     switch (ctx.args[0]) {
       case 'shards': {
-        const s = []
         const embed = new EmbedBuilder()
         embed.setFooter(ctx.t('commands:ping', { totalShard: ctx.client.shards.size }))
         embed.setColor('DEFAULT')
         ctx.client.shards.forEach(shard => {
-          let shardStatus
-          if (shard.status === 'ready') shardStatus = 'CONNECTED'
-          if (shard.status === 'disconnected') shardStatus = 'DISCONNECTED'
-          if (shard.status === 'connecting') shardStatus = 'CONNECTING'
-          if (shard.status === 'handshaking') shardStatus = 'HANDSHAKING'
-          s.push(embed.addField(`Shard: ${shard.id}`, `Ping: ${shard.latency} | ${shardStatus}`, true))
+          const shardStatus = shard.status === 'ready' ? ['CONNECTED', '<:online:518876154720026633>']
+            : shard.status === 'disconnected' ? ['OFFLINE', '<:offline:518876154782941187>']
+              : shard.status === 'connecting' ? ['CONNECTING', '<:dnd:518876154933936146>']
+                : ['HANDSHAKING', '<:idle:518876154912833549>']
+          embed.addField(`Shard ${shard.id} ${shardStatus[1]}`, `${shard.latency !== Infinity ? 'Ping: ' + shard.latency + 'ms |' : ''} ${shardStatus[0]}`)
         })
 
         ctx.send(embed)
         break
       }
       default: {
-        const ping = `Ping: \`${Math.round(ctx.message.channel.guild.shard.latency)}\`ms! | API Latency: \`${Date.now() - ctx.message.timestamp}\` | Shard: [${ctx.message.channel.guild.shard.id}/${ctx.client.shards.size}]`
+        const ping = `Ping: \`${Math.round(ctx.message.channel.guild.shard.latency)}\`ms! | API Latency: \`${Date.now() - ctx.message.timestamp}\` | Shard: [${ctx.message.channel.guild.shard.id}/${ctx.client.shards.size}] | Cluster ${process.env.CLUSTER_ID}`
         const msg = await ctx.send(':ping_pong:')
         await msg.edit(`:ping_pong:\n${ping}`)
       }
