328eeddceee91b3b28d7f43bdfc9f3cfd57efb84
mystery <38635037+MysteryMS@users.noreply.github.com>
2020-10-25 17:18:14 -0300

    when all this is done, it's done

--- src/structures/util/Helper.js
+++ src/structures/util/Helper.js
@@ -7,20 +7,19 @@ module.exports = class Helper {
     this.aliases = commandAliases
     this.usage = commandUsage
     this.description = commandDescription
-    this.t = context.t
   }
 
   help() {
     const usage = this.usage.split(' ')
     const commandName = '`' +  `${this.context.db.guild.prefix}${this.name}` + '` '
     const commandWithUsage = commandName + usage.map(element => '\`' + element + '\`').join(' ')
-    const embedDescription = `\n\n**${this.t('basic:howToUse')}** ${commandWithUsage}`
+    const embedDescription = `\n\n**${this.context.t('basic:howToUse')}** ${commandWithUsage}`
     const aliases = this.aliases.join(', ') || ':x:'
     const embed = new EmbedBuilder()
       .setTitle('`' + `${this.context.db.guild.prefix}${this.name}` + '`')
       .setColor('DEFAULT')
       .setDescription(this.description + embedDescription)
-      .addField(this.t('basic:aliases'), aliases)
+      .addField(this.context.t('basic:aliases'), aliases)
     return this.context.send(embed)
   }
 }
