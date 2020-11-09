ac4e11497b43b0c60467d091eca7353baa686f04
mystery <38635037+MysteryMS@users.noreply.github.com>
2020-11-09 16:03:37 -0300

    updates (read desc) * Add AvatarCommand.js * Add ChooseCommand.js; * Update locales * Add Axios

---
+++ src/commands/misc/AvatarCommand.js
@@ -0,0 +1,42 @@
+const Command = require('../../structures/command/Command')
+const EmbedBuilder = require('../../structures/util/EmbedBuilder')
+const axios = require('axios')
+
+module.exports = class AvatarCommand extends Command {
+  constructor() {
+    super({
+      name: 'avatar'
+    })
+  }
+
+  async run(ctx) {
+    const memberId = ctx.args[0]?.replace(/[<@!>]/g, '')
+    let member = ctx.client.users.get(memberId)
+
+    if (!member) {
+      const baseUrl = (userId, hash) => `https://cdn.discordapp.com/avatars/${userId}/${hash}.${hash.startsWith('a_') ? 'gif' :'png'}?size=2048`
+      try {
+        const request = await axios.get('https://discord.com/api/v8/users/' + memberId, {
+          headers: {
+            'Authorization': 'Bot ' + process.env.DISCORD_TOKEN
+          }
+        })
+        const response = request.data
+        member = {
+          username: response.username,
+          avatarURL: baseUrl(memberId, response.avatar)
+        }
+      } catch (e) {
+        member = ctx.message.author
+      }
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle(ctx.t('commands:avatar.userAvatar', { user: member.username }))
+      .setDescription(ctx.t('commands:avatar.download', { link: member.avatarURL }))
+      .setImage(member.avatarURL)
+      .setColor('DEFAULT')
+
+    ctx.send(embed)
+  }
+}
