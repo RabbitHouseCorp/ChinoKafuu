4e6d0dfc403419134f9f195c57530b5513b3692c
Yslan Ramos <yslan3000@gmail.com>
2020-11-13 04:09:15 -0300

    fix e no undef

--- src/commands/utils/CalculateCommand.js
+++ src/commands/utils/CalculateCommand.js
@@ -2,46 +2,45 @@ const Command = require('../../structures/command/Command')
 const workerpool = require('workerpool')
 const pool = workerpool.pool()
 
-function calc(expression) {
-	const { create, all } = require('mathjs')
-	const math = create(all)
+function calc (expression) {
+  const { create, all } = require('mathjs')
+  const math = create(all)
 
-	math.import({
+  math.import({
     import: 'Function import is disabled',
-	  createUnit: 'Function createUnit is disabled',
-	  evaluate: 'Function evaluate is disabled',
-	  parse: 'Function parse is disabled',
-	  simplify: 'Function simplify is disabled',
-	  derivative: 'Function derivative is disabled',
-	  format: 'Function format is disabled'
-	}, { override: true })
+    createUnit: 'Function createUnit is disabled',
+    evaluate: 'Function evaluate is disabled',
+    parse: 'Function parse is disabled',
+    simplify: 'Function simplify is disabled',
+    derivative: 'Function derivative is disabled',
+    format: 'Function format is disabled'
+  }, { override: true })
 
-	return math.eval(expression)
+  return math.eval(expression)
 }
 
 module.exports = class CalculateCommand extends Command {
-	constructor() {
-		super({
-      name: "calculate",
+  constructor () {
+    super({
+      name: 'calculate',
       arguments: 1,
-			aliases: ["calcular", "calc"],
-		})
+      aliases: ['calcular', 'calc']
+    })
   }
 
-	async run({ message, args  }, t) {
+  async run ({ message, args }, t) {
+    const expression = args.join(' ')
 
-    const expression = args.join(" ")
-
-		if (!expression) {
-      return message.reply("error", t("commands:calc.invalidArgs"))
+    if (!expression) {
+      return message.reply('error', t('commands:calc.invalidArgs'))
     }
 
     try {
       const result = await pool.exec(calc, [expression])
       pool.terminate()
-			message.reply("success", t("commands:calc.result", { result: result }))
+      message.reply('success', t('commands:calc.result', { result: result }))
     } catch (error) {
-      message.reply("error", t("commands:calc.result", { result: e.message }))
+      message.reply('error', t('commands:calc.result', { result: error.message }))
     }
-	}
+  }
 }
