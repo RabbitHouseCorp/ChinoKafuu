1da58b8d30fa6cb31b319846af2efa082c01a16a
DanielaGC <danielagc676@gmail.com>
2020-12-05 21:41:11 -0300

    Fix some bugs

--- src/commands/mod/AddEmojiCommand.js
+++ src/commands/mod/AddEmojiCommand.js
@@ -3,38 +3,39 @@ const fetch = require('node-fetch')
 const Helper = require('../../structures/util/Helper')
 
 module.exports = class AddEmojiCommand extends Command {
-  constructor() {
-    super({
-      name: 'addemoji',
-      aliases: ['adicionaremoji'],
-      permissions: [{
-        entity: 'both',
-        permissions: ['manageEmojis']
-      }]
-    })
-  }
-
-  async run(ctx) {
-    if (!ctx.args[0]) return new Helper(ctx, this.name, this.aliases, ctx._locale(`commands:${this.name}.usage`, `commands:${this.name}.description`)).help() //FIXME fix undefined in description pls :sob:
-    const url = ctx.args[0] ?? ctx.message.attachments[0]
-    let name = ctx.args[1]
-    if (!url) name = ctx.args[0]
-    if (!name || !url) return ctx.replyT('error', 'basic:missingArgs', {
-      prefix: ctx.db.guild.prefix,
-      commandName: this.name
-    }) //TODO Update to Helper
-    try {
-      const request = await fetch(url)
-      const buffer = await request.buffer()
-      const data = `data:image/${url.substr(url.length - 3)};base64,`
-      const base64Emoji = buffer.toString('base64')
-      return ctx.message.channel.guild.createEmoji({
-        name: name,
-        image: data + base64Emoji
-
-      }).then(emoji => ctx.send(emoji.name)).catch(() => ctx.replyT('error', 'commands:addemoji.error'))
-    } catch {
-      return ctx.replyT('error', 'commands:addemoji.error')
+    constructor() {
+        super({
+            name: 'addemoji',
+            aliases: ['adicionaremoji'],
+            arguments: 2,
+            permissions: [{
+                entity: 'both',
+                permissions: ['manageEmojis']
+            }]
+        })
     }
-  }
+
+    async run(ctx) {
+
+        const url = ctx.args[0] ?? ctx.message.attachments[0]
+        let name = ctx.args[1]
+        if (!url) name = ctx.args[0]
+        if (!name || !url) return ctx.replyT('error', 'basic:missingArgs', {
+            prefix: ctx.db.guild.prefix,
+            commandName: this.name
+        })
+        try {
+            const request = await fetch(url)
+            const buffer = await request.buffer()
+            const data = `data:image/${url.substr(url.length - 3)};base64,`
+            const base64Emoji = buffer.toString('base64')
+            return ctx.message.channel.guild.createEmoji({
+                name: name,
+                image: data + base64Emoji
+
+            }).then(emoji => ctx.send(emoji.name)).catch(() => ctx.replyT('error', 'commands:addemoji.error'))
+        } catch {
+            return ctx.replyT('error', 'commands:addemoji.error')
+        }
+    }
 }
--- src/structures/util/Helper.js
+++ src/structures/util/Helper.js
@@ -1,25 +1,26 @@
 const EmbedBuilder = require('../../structures/util/EmbedBuilder')
 
 module.exports = class Helper {
-  constructor(context, commandName, commandAliases, commandUsage, commandDescription) {
-    this.context = context
-    this.name = commandName
-    this.aliases = commandAliases
-    this.usage = commandUsage
-    this.description = commandDescription
-  }
+	constructor(context, commandName, commandAliases, commandUsage, commandDescription) {
+		this.context = context
+		this.name = commandName
+		this.aliases = commandAliases
+		this.usage = commandUsage
+		this.description = commandDescription
+	}
 
-  help() {
-    const usage = this.usage.split(' ')
-    const commandName = `${this.context.db.guild.prefix}${this.name}`
-    const commandWithUsage = `\`${commandName}\` ${usage.map(element => `\`${element}\``).join(' ')}`
-    const embedDescription = `\n\n**${this.context.t('basic:howToUse')}** ${commandWithUsage}`
-    const aliases = this.aliases.join(', ') || this.context.t('basic:noAliases')
-    const embed = new EmbedBuilder()
-      .setTitle(`\`${this.context.db.guild.prefix}${this.name}\``)
-      .setColor('DEFAULT')
-      .setDescription(`${this.description}${embedDescription}`)
-      .addField(this.context.t('basic:aliases'), aliases)
-    return this.context.send(embed)
-  }
+	help() {
+		const usage = this.usage.split(' ')
+		const commandName = `${this.context.db.guild.prefix}${this.name}`
+		const commandWithUsage = `\`${commandName}\` ${usage.map(element => `\`${element}\``).join(' ')}`
+		const embedDescription = `\n\n**${this.context._locale('basic:howToUse')}** ${commandWithUsage}`
+		const aliases = this.aliases.join(', ') || this.context._locale('basic:noAliases')
+		const embed = new EmbedBuilder()
+		embed.setTitle(`\`${this.context.db.guild.prefix}${this.name}\``)
+		embed.setColor('DEFAULT')
+		embed.setDescription(`${this.description}${embedDescription}`)
+		embed.addField(this.context._locale('basic:aliases'), aliases)
+
+		return this.context.send(embed)
+	}
 }
