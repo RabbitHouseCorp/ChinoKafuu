18176f17b0f5d6aa31ac43163d54d8567c284ce4
DanielaGC <danielagc676@gmail.com>
2020-12-06 18:25:57 -0300

    Add ServerIconCommand

---
+++ src/commands/utils/ServerIconCommand.js
@@ -0,0 +1,24 @@
+const { Command, EmbedBuilder } = require('../../utils')
+
+module.exports = class ServerIconCommand extends Command {
+    constructor() {
+        super({
+            name: 'servericon',
+            aliases: ['guildicon']
+        })
+    }
+
+    async run(ctx) {
+        const guild = ctx.message.channel.guild
+        if (!guild.icon) return ctx.replyT('error', 'commands:servericon.missingIcon')
+
+        const embed = new EmbedBuilder()
+        embed.setImage(guild.dynamicIconURL())
+        embed.setColor('DEFAULT')
+        embed.setDescription(ctx._locale('commands:servericon.download', { 0: guild.dynamicIconURL() }))
+        embed.setFooter(`©️ ${ctx.client.user.username}`)
+        embed.setTimestamp()
+
+        ctx.send(embed)
+    }
+}
