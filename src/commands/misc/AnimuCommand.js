101b9c571dfba7795918f535d23e5a0d8ff3abeb
DanielaGC <danielagc676@gmail.com>
2020-12-07 14:56:48 -0300

    Add animu radio module

---
+++ src/commands/misc/AnimuCommand.js
@@ -0,0 +1,101 @@
+const { Command, EmbedBuilder } = require('../../utils')
+const fetch = require('node-fetch')
+module.exports = class VoteCommand extends Command {
+    constructor() {
+        super({
+            name: 'animu',
+            aliases: ['moeanimu'],
+            arguments: 0,
+            permissions: [{
+                entity: 'bot',
+                permissions: ['embedLinks']
+            }]
+        })
+    }
+
+    async run(ctx) {
+        if (!ctx.message.member.voiceState.channelID) return ctx.replyT('error', 'basic:voice.authorAreNotInVoiceChannel')
+        
+        const argsNullEmbed = new EmbedBuilder()
+        argsNullEmbed.setColor('ANIMU')
+        argsNullEmbed.setTitle(ctx._locale('commands:animu.title'))
+        argsNullEmbed.addField(`${ctx.db.guild.prefix}animu join`, `**Usage:** ${ctx.db.guild.prefix}animu join\n**Aliases:** \`${ctx.db.guild.prefix}animu entrar, ${ctx.db.guild.prefix}animu tocar, ${ctx.db.guild.prefix}animu play\``)
+        argsNullEmbed.addField(`${ctx.db.guild.prefix}animu nowplaying`, `**Usage:** ${ctx.db.guild.prefix}animu nowplaying\n**Aliases:** \`${ctx.db.guild.prefix}animu np, ${ctx.db.guild.prefix}animu tocando\``)
+        argsNullEmbed.addField(`${ctx.db.guild.prefix}animu volume`, `**Usage:** ${ctx.db.guild.prefix}animu volume\n**Aliases:** \`${ctx.db.guild.prefix}animu vol\``)
+        argsNullEmbed.addField(`${ctx.db.guild.prefix}animu leave`, `**Usage:** ${ctx.db.guild.prefix}animu leave\n**Aliases:** \`${ctx.db.guild.prefix}animu sair, ${ctx.db.guild.prefix}animu parar, ${ctx.db.guild.prefix}animu stop\``)
+
+        if (!ctx.args[0]) return ctx.send(argsNullEmbed)
+        
+        const req = fetch('http://cast.animu.com.br:2199/rpc/animufm/streaminfo.get')
+        const res = req.then(res => res.json())
+        if (['play', 'join', 'tocar', 'entrar'].includes(ctx.args[0])) {
+            if (ctx.client.player.has(ctx.message.guildID)) return ctx.replyT('error', 'basic:voice.playerAlreadyPlaying')
+            const song = await ctx.client.lavalink.join(ctx.message.member.voiceState.channelID)
+            song.playAnimu()
+            ctx.client.player.set(ctx.message.guildID, song)
+            song.on('playNow', (track) => {
+                res.then(info => {
+                    const volume = ctx.client.player.get(ctx.message.guildID).player.state.volume
+                    const embed = new EmbedBuilder()
+                    embed.setColor('ANIMU')
+                    embed.setAuthor(track.info.title)
+                    embed.setThumbnail(info.data[0].track.imageurl)
+                    embed.addField(ctx._locale('commands:animu.nowPlaying'), info.data[0].song)
+                    embed.addField(ctx._locale('commands:animu.totalListening.title'), `${info.data[0].listenertotal} ${ctx._locale('commands:animu.totalListening.total')}`)
+                    embed.addField(ctx._locale('commands:animu.artist'), info.data[0].track.artist)
+                    embed.addField(ctx._locale('commands:animu.volume'), `${volume}/100`)
+
+                    ctx.send(embed)
+                })
+            })
+
+            song.on('playEnd', async () => {
+                await ctx.client.lavalink.manager.leave(ctx.message.guildID)
+                ctx.client.lavalink.manager.players.delete(ctx.message.guildID)
+                ctx.client.player.delete(ctx.message.guildID)
+            })
+
+            return
+        }
+
+        if (['volume', 'vol'].includes(ctx.args[0])) {
+            if (!ctx.message.channel.guild.members.get(ctx.client.user.id).voiceState.channelID) return ctx.replyT('error', 'baisc:voice.clientAreNotInVoiceChannel')
+            if (!ctx.client.player.has(ctx.message.guildID)) return ctx.replyT('error', 'basic:voice.playerNotFound')
+            if (!ctx.args[1]) return ctx.replyT('warn', 'commands:animu.currentVolume', { volume: ctx.client.player.get(ctx.message.guildID).player.state.volume })
+            if (parseInt(ctx.args[1]) > 100) return ctx.replyT('error', 'basic:voice.maxVolume')
+            if (parseInt(ctx.args[1]) < 5) return ctx.replyT('error', 'basic:voice.minVolume')
+
+            ctx.client.player.get(ctx.message.guildID).setVolume(ctx.args[1])
+            ctx.replyT('success', 'commands:animu.volumeChanged')
+
+            return
+        }
+
+        if (['nowplaying', 'tocandoagora', 'np'].includes(ctx.args[0])) {
+            res.then(info => {
+                const volume = ctx.client.player.get(ctx.message.guildID).player.state.volume
+                const embed = new EmbedBuilder()
+                embed.setColor('ANIMU')
+                embed.setAuthor(info.data[0].title)
+                embed.setThumbnail(info.data[0].track.imageurl)
+                embed.addField(ctx._locale('commands:animu.nowPlaying'), info.data[0].song)
+                embed.addField(ctx._locale('commands:animu.totalListening.title'), `${info.data[0].listenertotal} ${ctx._locale('commands:animu.totalListening.total')}`)
+                embed.addField(ctx._locale('commands:animu.artist'), info.data[0].track.artist)
+                embed.addField(ctx._locale('commands:animu.volume'), `${volume}/100`)
+
+                ctx.send(embed)
+            })
+
+            return
+        }
+
+        if (['stop', 'leave', 'parar', 'sair']) {
+            await ctx.client.lavalink.manager.leave(ctx.message.guildID)
+            ctx.client.lavalink.manager.players.delete(ctx.message.guildID)
+            ctx.client.player.delete(ctx.message.guildID)
+
+            ctx.replyT('success', 'commands:animu.leaving')
+            return
+        }
+    }
+}
--- src/listeners/GuildCreateListener.js
+++ src/listeners/GuildCreateListener.js
@@ -27,7 +27,7 @@ module.exports = class GuildCreateListener extends Listener {
             lang: this.region[guild.region]
         })
 
-        let t = client.i18nRegistry.getT(server.lang)
+        let _locale = client.i18nRegistry.getT(server.lang)
         let me = guild.members.get(client.user.id).permission.has('viewAuditLogs')
         if (!me) return
         let audit = await guild.getAuditLogs()
@@ -36,8 +36,8 @@ module.exports = class GuildCreateListener extends Listener {
         const embed = new EmbedBuilder()
         embed.setImage('https://cdn.discordapp.com/attachments/648188298149232644/770759671552016414/gc9DEF.png')
         embed.setColor('DEFAULT')
-        embed.setFooter(t('basic:addedToGuild.guildSaved', { 0: guild.name }), guild.icon ? guild.iconURL : null)
-        embed.addField(t('basic:addedToGuild.thanks'), t('basic:addedToGuild.description', { 0: user.mention, 1: guild.name, 2: server.prefix }))
+        embed.setFooter(_locale('basic:addedToGuild.guildSaved', { 0: guild.name }), guild.icon ? guild.iconURL : null)
+        embed.addField(_locale('basic:addedToGuild.thanks'), _locale('basic:addedToGuild.description', { 0: user.mention, 1: guild.name, 2: server.prefix }))
 
         user.getDMChannel().then(channel => channel.createMessage({ embed: embed }))
     }
