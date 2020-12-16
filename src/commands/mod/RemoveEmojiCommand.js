66da943cc4e50b5cb19eeab9e27ac0df625bb9e0
DanielaGC <danielagc676@gmail.com>
2020-12-16 18:25:23 -0300

    Added RemoveEmojiCommand

---
+++ src/commands/mod/RemoveEmojiCommand.js
@@ -0,0 +1,26 @@
+const { Command } = require('../../utils')
+
+module.exports = class RemoveEmojiCommand extends Command {
+    constructor() {
+        super({
+            name: 'removeemoji',
+            aliases: ['removeremoji'],
+            permissions: [{
+                entity: 'both',
+                permissions: ['manageEmojis']
+            }],
+            arguments: 1
+        })
+    }
+
+    run(ctx) {
+        const guild = ctx.message.channel.guild
+        const getEmoji = ctx.args[0].replace(/(<:)/, '').replace(/(<a:)/, '').replace(/(>)/, '').trim().split(':')
+        const emoji = guild.emojis.find(emoji => emoji.id === getEmoji[1])
+        if (!emoji) return ctx.replyT('error', 'commands:removeemoji.emojiNotFound')
+
+        guild.deleteEmoji(emoji.id).then(() => {
+            ctx.replyT('trash', 'commands:removeemoji.successfullyRemoved')
+        })
+    }
+}
