7f32af1b6a8656d64d22620fb82a0ee4eaf0234c
DanielaGC <danielagc676@gmail.com>
2020-12-16 19:47:01 -0300

    Added RemoveEmojiCommand

--- src/commands/mod/RemoveEmojiCommand.js
+++ src/commands/mod/RemoveEmojiCommand.js
@@ -17,7 +17,7 @@ module.exports = class RemoveEmojiCommand extends Command {
         const guild = ctx.message.channel.guild
         const getEmoji = ctx.args[0].replace(/(<:)/, '').replace(/(<a:)/, '').replace(/(>)/, '').trim().split(':')
         const emoji = guild.emojis.find(emoji => emoji.id === getEmoji[1])
-        if (!emoji) return ctx.replyT('error', 'commands:removeemoji.emojiNotFound')
+        if (!emoji) return ctx.replyT('error', 'basic:invalidEmoji')
 
         guild.deleteEmoji(emoji.id).then(() => {
             ctx.replyT('trash', 'commands:removeemoji.successfullyRemoved')
