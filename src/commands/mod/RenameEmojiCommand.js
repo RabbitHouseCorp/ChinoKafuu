9fdf70782b3b2a5cda6d923efd1c8a82b65886ea
DanielaGC <danielagc676@gmail.com>
2020-12-16 19:47:23 -0300

    Added RenameEmojiCommand

---
+++ src/commands/mod/RenameEmojiCommand.js
@@ -0,0 +1,26 @@
+const { Command } = require('../../utils')
+
+module.exports = class RenameEmojiCommand extends Command {
+    constructor() {
+        super({
+            name: 'renameemoji',
+            aliases: ['renomearemoji'],
+            permissions: [{
+                entity: 'both',
+                permissions: ['manageEmojis']
+            }],
+            arguments: 1
+        })
+    }
+
+    run(ctx) {
+        const guild = ctx.message.channel.guild
+        const getEmoji = ctx.args[0].replace(/(<:)/, '').replace(/(<a:)/, '').replace(/(>)/, '').trim().split(':')
+        const emoji = guild.emojis.find(emoji => emoji.id === getEmoji[1])
+        if (!emoji) return ctx.replyT('error', 'basic:invalidEmoji')
+
+        guild.editEmoji(emoji.id, { name: ctx.args.slice(1).join(' ') }).then(() => {
+            ctx.replyT('success', 'commands:renameemoji.successfullyRenamed')
+        })
+    }
+}
