556bdfdaf615127ea8138e0f967e5c91efe3c5b8
DanielaGC <danielagc676@gmail.com>
2020-12-22 22:33:38 -0300

    Fixed RoleColorCommand

--- src/commands/mod/RoleColorCommand.js
+++ src/commands/mod/RoleColorCommand.js
@@ -14,7 +14,7 @@ module.exports = class RoleColorCommand extends Command {
 		let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0])
 		if (!role) return message.chinoReply("error", t("commands:roleColor.invalidRole"))
 		if (role.rawPosition > message.guild.me.roles.highest.rawPosition) return message.chinoReply("error", t("commands:roleColor.higherRole"))
-		if (!args[1].startsWith("#")) return message.chinoReply("error", t("commands:roleColor.invalidHex"))
+		if (!args[1]?.startsWith("#")) return message.chinoReply("error", t("commands:roleColor.invalidHex"))
 		if (args[1].length < 7 || args[1].length > 7) return message.chinoReply("error", t("commands:roleColor.invalidHex"))
 
 		role.setColor(args[1]).then(message.chinoReply("success", t("commands:roleColor.success")))
