7a3a357d92054d6b0fd40b7cb2efc621e188c9cd
DanielaGC <danielagc676@gmail.com>
2021-01-02 18:47:32 -0300

    Added 'filterByCategory'

--- src/structures/command/CommandRegistry.js
+++ src/structures/command/CommandRegistry.js
@@ -1,13 +1,17 @@
 const Registry = require('../registry/Registry')
 
 module.exports = class CommandRegistry extends Registry {
-  constructor (path = __dirname + '/../../commands') {
-    super({ path, autoReload: process.env.ENABLE_REGISTRY_RELOAD || !process.env.PRODUCTION })
-    
-    this.loadAll(this.path)
-  }
+	constructor(path = __dirname + '/../../commands') {
+		super({ path, autoReload: process.env.ENABLE_REGISTRY_RELOAD || !process.env.PRODUCTION })
 
-  findByName (name) {
-    return this.findByProperty('name', name) || this.modules.filter((a) => a.aliases.includes(name))[0]
-  }
+		this.loadAll(this.path)
+	}
+
+	findByName(name) {
+		return this.findByProperty('name', name) || this.modules.filter((a) => a.aliases.includes(name))[0]
+	}
+
+	filterByCategory(category) {
+		return this.modules.filter((cmd) => cmd.__path.trim().split('/').includes(category))
+	}
 }
