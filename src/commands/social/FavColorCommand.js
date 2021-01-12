e145a7faa8ff8506cb15ba6bffee4844dd729ab1
DanielaGC <danielagc676@gmail.com>
2021-01-11 23:10:16 -0300

    Added FavColorCommand

---
+++ src/commands/social/FavColorCommand.js
@@ -0,0 +1,24 @@
+const { Command } = require('../../utils')
+
+module.exports = class FavColorCommand extends Command {
+    constructor() {
+        super({
+            name: 'favcolor',
+            aliases: ['favoritecolor', 'corfavorita'],
+            arguments: 1,
+            hasUsage: true
+        })
+    }
+
+    async run(ctx) {
+        if (ctx.db.user.yens < 150) return ctx.replyT('error', 'commands:favcolor.poorUser', { 0: ctx.db.user.yens - 75 })
+        if (!ctx.args[0].startsWith('#')) return ctx.replyT('error', 'commands:favcolor.invalidColor')
+
+        ctx.db.user.profileColor = ctx.args[0]
+        ctx.db.user.yens -= 150
+        ctx.db.user.save().then(() => {
+            ctx.replyT('success', 'commands:favcolor.successfullyChanged')
+        })
+
+    }
+}
