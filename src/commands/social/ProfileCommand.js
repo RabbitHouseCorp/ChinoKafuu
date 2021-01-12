ed58894e91cdb68e021745d6f923fedd1f852ef5
DanielaGC <danielagc676@gmail.com>
2021-01-11 23:10:34 -0300

    Added ProfileCommand

---
+++ src/commands/social/ProfileCommand.js
@@ -0,0 +1,42 @@
+const { Command } = require('../../utils')
+const axios = require('axios')
+module.exports = class ProfileCommand extends Command {
+    constructor() {
+        super({
+            name: 'profile',
+            aliases: ['perfil'],
+            hasUsage: true
+        })
+    }
+
+    async run(ctx) {
+        let member = await ctx.getUser(ctx.args[0])
+        if (!member) {
+            member = ctx.message.author
+        }
+
+        let user = await ctx.client.database.users.getOrCreate(member.id)
+        let couple = user.isMarry ? await ctx.getUser(user.marryWith) : { username: '', discriminator: '' }
+
+        console.log(couple?.username)
+        require('axios')({
+            url: "http://127.0.0.1:1234/render/profile",
+            method: 'post',
+            data: {
+                type: 'default',
+                name: member.username,
+                money: Number(user.yens).toLocaleString(),
+                aboutMe: user.aboutme,
+                married: user.isMarry,
+                partnerName: `${couple?.username}#${couple.discriminator}`,
+                bgId: 'no-game-no-live-2',
+                stickerId: 'bjork_post',
+                favColor: user.profileColor,
+                avatarUrl: member.avatarURL
+            },
+            responseType: 'arraybuffer'
+        }).then(jesus => {
+            ctx.send('', {}, { file: jesus.data, name: 'profile.png' })
+        })
+    }
+}
