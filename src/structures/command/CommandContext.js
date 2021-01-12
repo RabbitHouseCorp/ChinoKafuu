edf9b89cc627953517f690740e471c7c08a27a3b
DanielaGC <danielagc676@gmail.com>
2021-01-11 23:09:30 -0300

    Accept quote

--- src/structures/command/CommandContext.js
+++ src/structures/command/CommandContext.js
@@ -23,23 +23,13 @@ module.exports = class CommandContext {
      * @param options {object}
      * @returns {Promise<Eris.Message> | Promise<Eris.Message<Eris.TextableChannel>> | Promise<Eris.Message<Eris.TextChannel>> | Promise<Eris.Message<Eris.NewsChannel>> | Promise<Eris.Message<Eris.PrivateChannel>>}
      */
-    async send(content, options) {
+    async send(content, options, file) {
         if (typeof content === 'object') {
-            content = {
-                content: content.content,
-                embed: content.embed,
-                messageReferenceID: this.message.id,
-                options
-            }
 
-            return await this.message.channel.createMessage(content)
+            return await this.message.channel.createMessage(Object.assign(content, { messageReferenceID: this.message.id }, options), file)
         }
-        
-        return await this.message.channel.createMessage({
-            content,
-            messageReferenceID: this.message.id,
-            options
-        })
+
+        return await this.message.channel.createMessage(Object.assign({ content: content }, { messageReferenceID: this.message.id }, options), file)
     }
 
     /**
@@ -88,5 +78,92 @@ module.exports = class CommandContext {
             messageReferenceID: this.message.id,
             options
         })
+    }
+    /**
+     * 
+     * @param {string} args 
+     */
+    async getUser(args) {
+        if (!args) return false
+        try {
+            const member = await this.client.getRESTUser(args.replace(/[<@!>]/g, '')) || this.message.channel.guild.members.find((member) => member.user.username.toLowerCase().includes(args))
+
+            return member
+        } catch {
+            const member = this.message.channel.guild.members.find((member) => member.username.toLowerCase().includes(args.toLowerCase())) || this.message.channel.guild.members.find((member) => `${member.username}#${member.discriminator}`.toLowerCase() === args.toLowerCase())
+            if (!member) return false
+
+            return member.user
+        }
+
+    }
+
+    /**
+     * 
+     * @param {string} args 
+     */
+
+    async getEmoji(args) {
+        if (!args) return false
+        if (args.includes('%')) args = decodeURIComponent(args)
+        if (!args.includes(':')) {
+            const emoji = this.message.channel.guild.emojis.find(emoji => emoji.name.toLowerCase().includes(args.toLowerCase())) || this.message.channel.guild.emojis.get(args)
+            if (emoji) {
+                return {
+                    animated: emoji.animated,
+                    name: emoji.name,
+                    mention: `${emoji.animated ? '<a:' : '<:'}${emoji.name}:${emoji.id}>`,
+                    id: emoji.id,
+                    url: `https://cdn.discordapp.com/emojis/${emoji.id}.${emoji.animated ? 'gif' : 'png'}?v=1`
+                }
+            }
+
+            const axios = require('axios')
+            try {
+                if (await axios.get(`https://twemoji.maxcdn.com/2/72x72/${this.toUnicode(args).join('-')}.png`)) {
+                    return {
+                        animated: false,
+                        name: args,
+                        mention: args,
+                        id: this.toUnicode(args).join('-').toString(0),
+                        url: `https://twemoji.maxcdn.com/2/72x72/${this.toUnicode(args).join('-')}.png`
+                    }
+                } else {
+                    return false
+                }
+            } catch {
+                return false
+            }
+        }
+
+        const m = args.match(/<?(?:(a):)?(\w{2,32}):(\d{17,19})?>?/)
+        if (!m) return false
+        if (m[2] && !m[3]) return false
+
+        return {
+            animated: Boolean(m[1]),
+            name: m[2],
+            mention: `${Boolean(m[1]) ? '<a:' : '<:'}${m[2]}:${m[3]}>`,
+            id: m[3],
+            url: `https://cdn.discordapp.com/emojis/${m[3]}.${Boolean(m[1]) ? 'gif' : 'png'}?v=1`
+        }
+    }
+
+    /**
+     * 
+     * @param {string} text 
+     */
+    
+    toUnicode(text) {
+        let emojis = []
+        for (let codePoint of text) {
+            emojis.push(codePoint.codePointAt(0).toString(16))
+        }
+        return emojis
+    }
+
+    getRole(role) {
+        const getRole = this.message.channel.guild.roles.find(role => role.name.toLowerCase().includes(role.toLowerCase)) || this.message.channel.guild.roles.get(role.replace(/[<@&>]/g, ''))
+        if (!getRole) return false
     }
 }
