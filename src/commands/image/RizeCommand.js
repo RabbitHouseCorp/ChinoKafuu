035262d49831c23277fd198a394538a9fa832d65
DanielaGC <danielagc676@gmail.com>
2021-01-13 19:40:47 -0300

    Added LicenseCommand

---
+++ src/commands/image/RizeCommand.js
@@ -0,0 +1,29 @@
+const { Command } = require('../../utils')
+const axios = require('axios')
+
+module.exports = class RizeCommand extends Command {
+    constructor() {
+        super({
+            name: 'rize',
+            aliases: ['rizesign'],
+            hasUsage: true,
+            arguments: 1,
+            permissions: [{
+                permissions: ['attachFiles']
+            }]
+        })
+    }
+
+    async run(ctx) {
+        const buffer = await axios({
+            url: 'http://127.0.0.1:1234/render/rize',
+            method: 'post',
+            data: {
+                text: ctx.args.join(' ')
+            },
+            responseType: 'arraybuffer'
+        })
+
+        ctx.send('', {}, {file: buffer.data, name: 'rize.png'})
+    }
+}
