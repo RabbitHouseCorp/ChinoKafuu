8c297f76c398780c98536761eb2331c2b73dde6e
DanielaGC <danielagc676@gmail.com>
2021-01-13 19:40:19 -0300

    Added LaranjoCommand

---
+++ src/commands/image/LaranjoCommand.js
@@ -0,0 +1,28 @@
+const { Command } = require('../../utils')
+const axios = require('axios')
+
+module.exports = class LaranjoCommand extends Command {
+    constructor() {
+        super({
+            name: 'laranjo',
+            hasUsage: true,
+            arguments: 1,
+            permissions: [{
+                permissions: ['attachFiles']
+            }]
+        })
+    }
+
+    async run(ctx) {
+        const buffer = await axios({
+            url: 'http://127.0.0.1:1234/render/laranjo',
+            method: 'post',
+            data: {
+                text: ctx.args.join(' ')
+            },
+            responseType: 'arraybuffer'
+        })
+
+        ctx.send('', {}, { file: buffer.data, name: 'laranjo.png' })
+    }
+}
