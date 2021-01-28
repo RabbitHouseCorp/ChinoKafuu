5bf42b1ec6fedbcc7a5e59457f96ea7336038db6
DanielaGC <danielagc676@gmail.com>
2021-01-28 11:09:35 -0300

    Update emojis

--- src/utils/Emojis.js
+++ src/utils/Emojis.js
@@ -20,5 +20,8 @@ module.exports = {
     ring_couple: '<:ring_couple:791360830221844512>',
     broken_heart: '💔',
     chino_maid: '<:chino_maid:675947319232823326>',
-    tippy: '<a:tippy:796787333084348416>'
+    tippy: '<a:tippy:796787333084348416>',
+    chino_upset: '<:chino_upset:682017961149464645>',
+    chino_tea: '<:chino_tea:545430851895427080>',
+    chino_kek: '<:chino_kek:594995063747117083>'
 }
