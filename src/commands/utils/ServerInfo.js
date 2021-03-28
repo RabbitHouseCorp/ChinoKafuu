f35068ff98a0acff8e52d640866463bc05bab099
DanielaGC <danielagc676@gmail.com>
2021-03-27 21:14:41 -0300

    Fix cache problem with the guild owner

--- src/commands/utils/ServerInfo.js
+++ src/commands/utils/ServerInfo.js
@@ -1,7 +1,7 @@
 const { Command, EmbedBuilder } = require('../../utils')
 const moment = require('moment')
 module.exports = class ServerInfoCommand extends Command {
-  constructor () {
+  constructor() {
     super({
       name: 'serverinfo',
       aliases: ['guildinfo'],
@@ -12,9 +12,10 @@ module.exports = class ServerInfoCommand extends Command {
     })
   }
 
-  async run (ctx) {
+  async run(ctx) {
     moment.locale(ctx.db.guild.lang)
     const guild = ctx.message.channel.guild
+    const owner = await ctx.getUser(guild.ownerID)
     const _locale = ctx._locale
     const memberCount = {
       userCount: guild.members.filter(member => !member.user.bot).length,
@@ -36,7 +37,7 @@ module.exports = class ServerInfoCommand extends Command {
     embed.setThumbnail(guild.icon ? guild.iconURL : 'https://cdn.discordapp.com/attachments/468878707449397258/785277583411118080/PicsArt_12-06-07.52.13.jpg')
     embed.setImage(guild.splash ? guild.splashURL : null)
     embed.addField(_locale('commands:serverinfo.guildID'), guild.id, true)
-    embed.addField(_locale('commands:serverinfo.guildOwner'), `${guild.members.get(guild.ownerID).username}#${guild.members.get(guild.ownerID).discriminator}`, true)
+    embed.addField(_locale('commands:serverinfo.guildOwner'), `${owner.username}#${owner.discriminator}`, true)
     embed.addField(_locale('commands:serverinfo.guildRegion'), _locale(`commands:serverinfo.region.${guild.region}`), true)
     embed.addField(_locale('commands:serverinfo.guildAFKChannel.title'), guild.channels.get(guild.afkChannelID) ?? _locale('commands:serverinfo.guildAFKChannel.noAfkChannel'), true)
     embed.addField(_locale('commands:serverinfo.booster.title'), `**${_locale('commands:serverinfo.booster.levelCount')}:** ${guild.premiumTier}\n**${_locale('commands:serverinfo.booster.boosterCount')}:** ${guild.premiumSubscriptionCount}`, true)
