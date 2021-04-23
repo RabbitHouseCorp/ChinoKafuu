2d48719f15424437e4f0b711549e85e1a143e50b
DanielaGC <danielagc676@gmail.com>
2021-04-22 22:48:11 -0300

    Some improvements

--- src/structures/Bot.js
+++ src/structures/Bot.js
@@ -4,10 +4,9 @@ const CommandRegistry = require('./command/CommandRegistry')
 const I18NRegistry = require('./i18n/I18NRegistry')
 const ClusteringInterface = require('./util/ClusteringInterface')
 const Database = require('./database/Database')
-const PolluxClient = require('./util/PolluxClient')
 
 module.exports = class Bot extends Client {
-  constructor (...data) {
+  constructor(...data) {
     super(...data)
 
     /**
