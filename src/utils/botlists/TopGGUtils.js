65cacdd308b8f151c8f32d8a87ce912b6eb8007a
DanielaGC <danielagc676@gmail.com>
2021-04-25 19:25:13 -0300

    Whoops, don't try post if the token is null

--- src/utils/botlists/TopGGUtils.js
+++ src/utils/botlists/TopGGUtils.js
@@ -1,15 +1,17 @@
 const { Api } = require('@top-gg/sdk')
 
 module.exports = class TopGGUtils extends Api {
-  constructor(token) {
+  constructor(token = null) {
     super(token)
+    this.token = token
   }
 
-  getVote(id) {
-    return this.hasVoted(id)
+  async getVote(id) {
+    return await this.hasVoted(id)
   }
 
   async post(client) {
+    if (!this.token) return
     await this.postStats({
       serverCount: client.guilds.size,
       shardCount: client.shards.size
