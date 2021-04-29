11f7be7961d11b4ebdfd82b0f4f0bc5264098a65
DanielaGC <danielagc676@gmail.com>
2021-04-29 17:41:57 -0300

    Oof

--- src/utils/botlists/TopGGUtils.js
+++ src/utils/botlists/TopGGUtils.js
@@ -2,10 +2,7 @@ const { Api } = require('@top-gg/sdk')
 
 module.exports = class TopGGUtils extends Api {
   constructor() {
-    if (process.env.TOPGG_TOKEN) {
-      super(process.env.TOPGG_TOKEN)
-    }
-    
+    super(process.env.TOPGG_TOKEN)
     this.token = process.env.TOPGG_TOKEN ? process.env.TOPGG_TOKEN : null
   }
 
