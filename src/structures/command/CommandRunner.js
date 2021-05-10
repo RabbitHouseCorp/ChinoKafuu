9db6d3957957e28842b10ba1f401e881b8569748
DanielaGC <danielagc676@gmail.com>
2021-05-10 15:29:54 -0300

    add(CommandRunner) added auto invite if someone send a invite on bot dm

--- src/structures/command/CommandRunner.js
+++ src/structures/command/CommandRunner.js
@@ -5,10 +5,31 @@ const EmbedBuilder = require('../../structures/util/EmbedBuilder')
 module.exports = class CommandRunner {
   static async run(client, message) {
     if (message.author.bot) return
-    if (message.channel.type !== 0) return
+    if (message.channel.type !== 0) {
+      const isInvite = (/((?:discord\.gg|discordapp\.com\/invite|discord\.com\/invite|discord\.me|discord\.io))/g).test(message.content)
+      if (isInvite) {
+        const dmChannel = await message.author.getDMChannel()
+        const text = message.content.trim().split(' ')
+        const findInvite = text.find(invite => invite.includes('discord.gg'))
+          .replace(/(https:\/\/)?(http:\/\/)/g, '')
+          .replace(/(discord\.gg|discordapp\.com\/invite|discord\.com\/invite|discord\.me|discord\.io)/g, '')
+          .replace(/(\/)/g, '')
+        const invite = await client.getInvite(findInvite)
+        const embed = new EmbedBuilder()
+        embed.setColor('DEFAULT')
+        embed.setAuthor(message.author.username, message.author.avatarURL)
+        embed.setThumbnail(invite.guild.iconURL)
+        embed.setDescription(`Hey, here is my invite to adding me on \`${invite.guild.name}\`:\n\n[Minimal permissions](https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=71158976&guild_id=${invite.guild.id})\n[Recommended permissions](https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2117578239&guild_id=${invite.guild.id})`)
+        dmChannel.createMessage(embed.build())
+        return
+      }
+
+      return
+    }
+
     const userData = await client.database.users.getOrCreate(message.author.id, { shipValue: Math.floor(Math.random() * 55) })
 
-    const guildData = await client.database.guilds.getOrCreate(message.guildID)
+    const guildData = await message.channel.type !== 0 ?client.database.guilds.getOrCreate(message.guildID) : null
     if (guildData.blacklist) {
       return client.leaveGuild(message.guildID)
     }
