4a02acf2d8c5de6bc47c04f5f75a4caffe9359d7
DanielaGC <danielagc676@gmail.com>
2021-08-07 23:44:40 -0300

    fix(GuildMemberRemoveListener)

--- src/listeners/guilds/GuildMemberRemoveListener.js
+++ src/listeners/guilds/GuildMemberRemoveListener.js
@@ -12,25 +12,26 @@ module.exports = class GuildMemberRemoveListener extends Listener {
       const _locale = client.i18nRegistry.getT(server.lang)
       const audit = await guild.getAuditLogs()
       const guildBanAdd = audit.entries.filter(action => action.actionType === 20)
-      if (guildBanAdd[0].user.id === client.user.id) return
-      if (!guildBanAdd[0]) return
-      const mod = guildBanAdd[0].user
-      const reason = guildBanAdd[0].reason ?? _locale('basic:noReason')
-      const embed = new EmbedBuilder()
-      embed.setColor('MODERATION')
-      embed.setThumbnail(member.avatarURL)
-      embed.setTitle(_locale('basic:punishment.kicked', { 0: `${member.username}#${member.discriminator}` }))
-      embed.addField(_locale('basic:punishment.embed.memberName'), `${member.username}#${member.discriminator} (\`${member.id}\`)`)
-      embed.addField(_locale('basic:punishment.embed.staffName'), `${mod.username}#${mod.discriminator} (\`${mod.id}\`)`)
-      embed.addField(_locale('basic:punishment.embed.reason'), reason)
+      if (guildBanAdd[0]) {
+        if (guildBanAdd[0].user.id === client.user.id) return
+        const mod = guildBanAdd[0].user
+        const reason = guildBanAdd[0].reason ?? _locale('basic:noReason')
+        const embed = new EmbedBuilder()
+        embed.setColor('MODERATION')
+        embed.setThumbnail(member.avatarURL)
+        embed.setTitle(_locale('basic:punishment.kicked', { member: `${member.username}#${member.discriminator}` }))
+        embed.addField(_locale('basic:punishment.embed.memberName'), `${member.username}#${member.discriminator} (\`${member.id}\`)`)
+        embed.addField(_locale('basic:punishment.embed.staffName'), `${mod.username}#${mod.discriminator} (\`${mod.id}\`)`)
+        embed.addField(_locale('basic:punishment.embed.reason'), reason)
 
-      if (!server.punishModule) return
-      const channel = guild.channels.get(server.punishChannel)
-      if (!channel) {
-        server.punishModule = false
-        server.punishChannel = ''
-        server.save()
-        return
+        if (!server.punishModule) return
+        const channel = guild.channels.get(server.punishChannel)
+        if (!channel) {
+          server.punishModule = false
+          server.punishChannel = ''
+          server.save()
+          return
+        }
       }
 
       channel.createMessage(embed.build())
@@ -41,7 +42,7 @@ module.exports = class GuildMemberRemoveListener extends Listener {
       const embed = new EmbedBuilder()
       embed.setColor('MODERATION')
       embed.setThumbnail(member.avatarURL)
-      embed.setTitle(_locale('basic:punishment.kicked', { 0: `${member.username}#${member.discriminator}` }))
+      embed.setTitle(_locale('basic:punishment.kicked', { member: `${member.username}#${member.discriminator}` }))
       embed.addField(_locale('basic:punishment.embed.memberName'), `${member.username}#${member.discriminator} (\`${member.id}\`)`)
       embed.addField(_locale('basic:punishment.embed.reason'), reason)
 
