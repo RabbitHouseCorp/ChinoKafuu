cd42b5b182e774994e555bab5cdc524a426e8d88
Navy Cake <navycakedeveloper@gmail.com>
2021-08-11 01:01:53 -0300

    Load the entire list.

--- src/listeners/slash/SlashCommand.js
+++ src/listeners/slash/SlashCommand.js
@@ -18,7 +18,7 @@ module.exports = class SlashCommand extends Listener {
             }
 
             // commandRegistry:
-            client.slashCommand.createCommand(commandFolder)
+            client.slashCommand.addVolumeOfCommands(commandFolder)
         }
 
     }
