f5c875a599cc156e36044d19c4792aca41849426
DanielaGC <danielagc676@gmail.com>
2021-08-14 14:50:27 -0300

    Migrate congaparrot

--- src/commands/slash/fun/ParrotCommand.js
+++ src/commands/slash/fun/ParrotCommand.js
@@ -16,15 +16,17 @@ module.exports = class ParrotCommand extends Command {
         .setDescription('Sends funny parrots')
         .addOptions(
           new CommandOptions()
-            .setType(6)
-            .setName('user')
-            .setDescription('Mention the member on the server'),
+            .setType(4)
+            .setName('quantity')
+            .setDescription('Quantity of parrots.')
+            .isRequired()
         )
     })
   }
 
   async run(ctx) {
-    if (ctx.args[0] > 20) return ctx.replyT('error', ctx._locale('commands:congaparrot.maxAllowed'))
-    ctx.send('<a:parrot_dance:554489834417291285>'.repeat(ctx.args[0]))
+    const quantity = ctx.message.command.interface.get('quantity').value
+    if (quantity > 20) return ctx.replyT('error', ctx._locale('commands:congaparrot.maxAllowed'))
+    ctx.send('<a:parrot_dance:554489834417291285>'.repeat(quantity))
   }
 }
