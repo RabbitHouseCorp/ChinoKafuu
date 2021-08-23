9c10202001c9bc3c36a0c9f10b0dc1ab9783d2c3
DanielaGC <danielagc676@gmail.com>
2021-08-23 01:19:18 -0300

    Migrate RemoveEmojiCommand

--- src/commands/slash/mod/RemoveEmojiCommand.js
+++ src/commands/slash/mod/RemoveEmojiCommand.js
@@ -13,44 +13,22 @@ module.exports = class RemoveEmojiCommand extends Command {
       }],
       arguments: 1,
       slash: new CommandBase()
-        .setName('remove')
+        .setName('removeemoji')
         .setDescription('Removes an emoji in the current guild.')
         .addOptions(
           new CommandOptions()
-            .setType(1)
+            .setType(3)
             .setName('emoji')
             .setDescription('Removes an emoji in the current guild.')
-            .addOptions(
-              new CommandOptions()
-                .setType(3)
-                .setName('input')
-                .setDescription('Enter the emoji you are adding to the server.')
-                .isRequired()
-            ),
-          new CommandOptions()
-            .setType(1)
-            .setName('role')
-            .setDescription('Removes a role from a guild member.')
-            .addOptions(
-              new CommandOptions()
-                .setType(6)
-                .setName('user')
-                .setDescription('Mention member on server.')
-                .isRequired(),
-              new CommandOptions()
-                .setType(7)
-                .setName('role')
-                .setDescription('Mention role on server.')
-                .isRequired()
-            ),
+            .isRequired()
         )
     })
   }
 
-  run(ctx) {
+  async run(ctx) {
     const guild = ctx.message.guild
-    const getEmoji = ctx.args[0].replace(/(<:)/, '').replace(/(<a:)/, '').replace(/(>)/, '').trim().split(':')
-    const emoji = guild.emojis.find(emoji => emoji.id === getEmoji[1])
+    const getEmoji = await ctx.getEmoji(ctx.message.command.interface.get('emoji').value)
+    const emoji = guild.emojis.find(emoji => emoji.id === getEmoji.id)
     if (!emoji) return ctx.replyT('error', 'basic:invalidEmoji')
 
     guild.deleteEmoji(emoji.id).then(() => {
