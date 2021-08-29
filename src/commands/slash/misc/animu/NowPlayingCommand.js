69c6dfd5a24ca6ba1f5a38d7eaf718f2f7fd55aa
Navy <43297329+NavyCake@users.noreply.github.com>
2021-08-28 21:49:52 -0300

    Migrating AnimuCommand to SlashCommand

---
+++ src/commands/slash/misc/animu/NowPlayingCommand.js
@@ -0,0 +1,33 @@
+const { Command, EmbedBuilder } = require('../../../utils')
+const axios = require('axios')
+
+module.exports = class AnimuNowPlayingCommand extends Command {
+    constructor() {
+        super({
+            name: 'animu nowplaying',
+            aliases: ['module', 'configurações', 'configurar'],
+            permissions: [{
+                entity: 'bot',
+                permissions: ['embedLinks']
+            }]
+        })
+    }
+
+    async run(ctx) {
+        const res = await axios.get('https://cast.animu.com.br:9000/api/v2/history/?format=json&limit=1&offset=0&server=1')
+        if (!ctx.message.guild.members.get(ctx.client.user.id).voiceState.channelID) return ctx.replyT('error', 'basic:voice.clientAreNotInVoiceChannel')
+        if (!ctx.client.player.has(ctx.message.guildID)) return ctx.replyT('error', 'basic:voice.playerNotFound')
+        const volume = ctx.client.player.get(ctx.message.guildID).player.state.volume
+        const embed = new EmbedBuilder()
+        embed.setColor('ANIMU')
+        embed.setAuthor('Rádio Animu')
+        embed.setThumbnail(res.data.results[0].img_medium_url)
+        embed.addField(ctx._locale('commands:animu.nowPlaying'), res.data.results[0].metadata)
+        embed.addField(ctx._locale('commands:animu.totalListening.title'), `${res.data.results[0].n_listeners} ${ctx._locale('commands:animu.totalListening.total')}`)
+        embed.addField(ctx._locale('commands:animu.artist'), res.data.results[0].author)
+        embed.addField('DJ', res.data.results[0].dj_name)
+        embed.addField(ctx._locale('commands:animu.volume'), `${volume}/100`)
+
+        ctx.send(embed.build())
+    }
+}
