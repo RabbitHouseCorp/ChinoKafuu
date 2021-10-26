b91f5e34f0499b6686374e643c3523cdb46901f9
Nayv <navycakedeveloper@gmail.com>
2021-10-26 04:30:16 -0300

    fix

--- src/manager/BotInterface.js
+++ src/manager/BotInterface.js
@@ -1,34 +1,33 @@
-const Bot = require("../structures/Bot");
-const Logger = require("../structures/util/Logger");
+const Bot = require('../structures/Bot');
+const Logger = require('../structures/util/Logger');
 module.exports = class BotInterface {
 
-
-    async spawnShards(pluginManager) {
-        this.shardManager = new Bot(process.env.DISCORD_TOKEN, {
-            maxShards: parseInt(process.env.SHARD_AMOUNT),
-            compress: true,
-            defaultImageFormat: 'png',
-            defaultImageSize: 2048,
-            restMode: true,
-            allowedMentions: {
-                everyone: false,
-                roles: false,
-                users: true,
-                repliedUser: true
-            },
-            intents: 14079
-        })
-        this.shardManager.pluginManager = pluginManager.$pluginManager
-        this.shardManager.database = pluginManager.$pluginManager.pluginStore.get('mongodb')?.classState ?? undefined
-        this.shardManager.lavalink = pluginManager.$pluginManager.pluginStore.get('lavalink')?.classState ?? undefined
-        this.shardManager.player = new Map()
-        try {
-            await this.shardManager.connect().then(() => {
-                Logger.debug('Successfully connected to Discord\'s gateway.')
-            })
-        } catch (e) {
-            console.log(e)
-        }
+  async spawnShards(pluginManager) {
+    this.shardManager = new Bot(process.env.DISCORD_TOKEN, {
+      maxShards: parseInt(process.env.SHARD_AMOUNT),
+      compress: true,
+      defaultImageFormat: 'png',
+      defaultImageSize: 2048,
+      restMode: true,
+      allowedMentions: {
+        everyone: false,
+        roles: false,
+        users: true,
+        repliedUser: true
+      },
+      intents: 14079
+    })
+    this.shardManager.pluginManager = pluginManager.$pluginManager
+    this.shardManager.database = pluginManager.$pluginManager.pluginStore.get('mongodb')?.classState ?? undefined
+    this.shardManager.lavalink = pluginManager.$pluginManager.pluginStore.get('lavalink')?.classState ?? undefined
+    this.shardManager.player = new Map()
+    try {
+      await this.shardManager.connect().then(() => {
+        Logger.debug('Successfully connected to Discord\'s gateway.')
+      })
+    } catch (e) {
+      console.log(e)
     }
+  }
 }
 
