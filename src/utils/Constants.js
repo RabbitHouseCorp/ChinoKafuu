698f0d997bc762c82c41ae5b4143a55e55aa2e6e
Nayv <43297329+nayvcake@users.noreply.github.com>
2021-10-26 02:47:37 -0300

    Add flag to constants

---
+++ src/utils/Constants.js
@@ -0,0 +1,32 @@
+module.exports.Flags_Guild = {
+  GUILD_TESTER: 1 << 0,
+  PREMIUM: 1 << 1,
+  COMMAND_ACCESS_TESTER: 1 << 2,
+  PARTNER: 1 << 3,
+  BLACKLIST: 1 << 4,
+  VERIFIED: 1 << 5
+}
+
+module.exports.Flags_Users = {
+  DEVELOPER: 1 << 0,
+  BUG_HUNTER: 1 << 1,
+  BUG_HUNTER_EXTREME: 1 << 2,
+  VOTE_ACTIVE: 1 << 4,
+  TRANSLATOR: 1 << 5,
+  VERIFIED: 1 << 6,
+  STORE_ACCESS: 1 << 3,
+  ACCESS_TO_BUY_PROFILE: 1 << 7,
+  ACCESS_TO_DECORATE_PROFILE: 1 << 8,
+  BOOST_ACTIVE: 1 << 9,
+}
+
+module.exports.Flags_Command = {
+  BROKEN_COMMAND: 1 << 0,
+  BROKEN_COMMAND_NOTICED: 1 << 1,
+  BUG_TRACKING: 1 << 2,
+  DISABLED: 1 << 3,
+  COMMAND_TESTER: 1 << 4
+}
+
+
+module.exports.
