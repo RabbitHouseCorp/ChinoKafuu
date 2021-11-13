4f53a30d6af5ec759aa89e244ec42ec338bf72f5
Nayv <navycakedeveloper@gmail.com>
2021-11-13 18:50:32 -0300

    Mechanism

---
+++ src/utils/ResultsMechanism.js
@@ -0,0 +1,62 @@
+module.exports = class ResultsMechanism {
+  searchChannel_Interaction(search, interaction) {
+    let channels = []
+
+    for (const channelPosition of interaction.channel.guild.channels) {
+      const channel = channelPosition[1]
+      const p = search.split('')
+      const y = channel.name.replace('-', '').split('')
+
+      let confirm = 0
+      let wrong = false;
+
+      for (const k of p) {
+        const letter = y.indexOf(k)
+
+        if (!wrong) {
+          if (y[letter] == undefined) {
+            wrong = true;
+            wrong++;
+          } else {
+            confirm++;
+          }
+        }
+      }
+
+      if (!wrong) {
+        if (confirm > 0) {
+          channels.push({
+            type: 3,
+            name: `${channel.name} - ${channel.id}`,
+            value: channel.id
+          })
+        }
+      }
+      if (channel.name.replace('-', '') == search) {
+        channels = []
+        channels.push({
+          type: 3,
+          name: `${channel.name} - ${channel.id}`,
+          value: channel.id
+        })
+        break
+      }
+      if (channel.id == search) {
+        channels = []
+        channels.push({
+          type: 3,
+          name: `${channel.name} - ${channel.id}`,
+          value: channel.id
+        })
+        break
+      }
+    }
+
+    if (channels.size == 0) {
+      interaction.autoComplete.addOptions().callback().then((l) => console.log(l))
+    } else {
+      interaction.autoComplete.addOptions(channels).callback().then((l) => console.log(l))
+
+    }
+  }
+}
