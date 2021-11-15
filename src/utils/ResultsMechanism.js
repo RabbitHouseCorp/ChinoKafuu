aae8635ebfd33cd3a83d5ca032ef651bebf2a579
Kali <kali@pop-os.localdomain>
2021-11-15 02:27:12 -0300

    Implement more method to support command update.

--- src/utils/ResultsMechanism.js
+++ src/utils/ResultsMechanism.js
@@ -1,3 +1,5 @@
+const { Constants } = require('eris')
+
 module.exports = class ResultsMechanism {
   searchChannel_Interaction(search, interaction) {
     let channels = []
@@ -57,6 +59,139 @@ module.exports = class ResultsMechanism {
     } else {
       interaction.autoComplete.addOptions(channels).callback().then((l) => console.log(l))
 
+    }
+  }
+
+  searchTextChannel_Interaction(search, interaction) {
+    let channels = []
+
+    for (const channelPosition of interaction.channel.guild.channels) {
+      const channel = channelPosition[1]
+      const p = search.split('')
+      const y = channel.name.replace('-', '').split('')
+
+      let confirm = 0
+      let wrong = false;
+
+      for (const k of p) {
+        const letter = y.indexOf(k)
+
+        if (!wrong) {
+          if (y[letter] == undefined) {
+            wrong = true;
+            wrong++;
+          } else {
+            confirm++;
+          }
+        }
+      }
+
+      if (!wrong) {
+        if (confirm > 0) {
+          if (channel.type == Constants.ChannelTypes.GUILD_TEXT) {
+            channels.push({
+              type: 3,
+              name: `${channel.name} - ${channel.id}`,
+              value: channel.id
+            })
+          }
+        }
+      }
+      if (channel.name.replace('-', '') == search) {
+        if (channel.type == Constants.ChannelTypes.GUILD_TEXT) {
+          channels = []
+          channels.push({
+            type: 3,
+            name: `${channel.name} - ${channel.id}`,
+            value: channel.id
+          })
+        }
+        break
+      }
+      if (channel.id == search) {
+        if (channel.type == Constants.ChannelTypes.GUILD_TEXT) {
+          channels = []
+          channels.push({
+            type: 3,
+            name: `${channel.name} - ${channel.id}`,
+            value: channel.id
+          })
+        }
+        break
+      }
+    }
+
+    if (channels.size == 0) {
+      interaction.autoComplete.addOptions().callback().then((l) => console.log(l))
+    } else {
+      interaction.autoComplete.addOptions(channels).callback().then((l) => console.log(l))
+
+    }
+  }
+
+  searchVoicehannel_Interaction(search, interaction) {
+    let channels = []
+
+    for (const channelPosition of interaction.channel.guild.channels) {
+      const channel = channelPosition[1]
+      const p = search.split('')
+      const y = channel.name.replace('-', '').split('')
+
+      let confirm = 0
+      let wrong = false;
+
+      for (const k of p) {
+        const letter = y.indexOf(k)
+
+        if (!wrong) {
+          if (y[letter] == undefined) {
+            wrong = true;
+            wrong++;
+          } else {
+            confirm++;
+          }
+        }
+      }
+
+      if (!wrong) {
+        if (confirm > 0) {
+          if (channel.type == Constants.ChannelTypes.GUILD_VOICE) {
+            channels.push({
+              type: 3,
+              name: `${channel.name} - ${channel.id}`,
+              value: channel.id
+            })
+          }
+        }
+      }
+      if (channel.name.replace('-', '') == search) {
+        if (channel.type == Constants.ChannelTypes.GUILD_VOICE) {
+          channels = []
+          channels.push({
+            type: 3,
+            name: `${channel.name} - ${channel.id}`,
+            value: channel.id
+          })
+        }
+        break
+      }
+      if (channel.id == search) {
+        if (channel.type == Constants.ChannelTypes.GUILD_VOICE) {
+          channels = []
+          channels.push({
+            type: 3,
+            name: `${channel.name} - ${channel.id}`,
+            value: channel.id
+          })
+        }
+        break
+      }
+    }
+
+    if (channels.size == 0) {
+      interaction.autoComplete.addOptions().callback()
+    } else {
+      interaction.autoComplete.addOptions(channels).callback()
     }
   }
 }
