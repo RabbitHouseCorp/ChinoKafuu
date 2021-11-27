e62c1002345efdaa7310b8d77d714661ab9ca420
aetrnyx <aetrnyx@gmail.com>
2021-11-28 02:12:02 +0530

    Added some fixes

--- src/utils/plugins/store/BotStore.js
+++ src/utils/plugins/store/BotStore.js
@@ -1,33 +1,30 @@
-const PluginExtend = require('../loaders/PluginExtend');
+const PluginExtend = require("../loaders/PluginExtend");
 
 module.exports = class BotStore extends PluginExtend {
   constructor() {
     super({
-      name: 'bot',
+      name: "bot",
       args: {},
-      timeout: 50 * 1000
-    })
-
+      timeout: 50 * 1000,
+    });
   }
 
   start() {
     try {
-
-      if (!process.env.CLUSTERS) {
-        const Manager = require('./../../../sharder/manager/Manager')
-        const manager = new Manager()
-        manager.start()
-
+      if (process.env.CLUSTERS === "true") {
+        const Manager = require("./../../../sharder/manager/Manager");
+        const manager = new Manager();
+        manager.start();
       } else {
-        const BotInterface = require('./../../../manager/BotInterface');
+        const BotInterface = require("./../../../manager/BotInterface");
 
-        this.$addClassState({ data: new BotInterface().spawnShards(this) })
+        this.$addClassState({ data: new BotInterface().spawnShards(this) });
       }
 
-      this.ready()
+      this.ready();
     } catch (err) {
-      console.log(err)
-      this.fail(err)
+      console.log(err);
+      this.fail(err);
     }
   }
-}
+};
