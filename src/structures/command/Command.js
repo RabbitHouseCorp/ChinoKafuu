baa1d7a05fb85ffa49fabfa1b01873b589010ba5
Nayv <43297329+nayvcake@users.noreply.github.com>
2021-12-24 13:36:33 -0300

    Command Callback

--- src/structures/command/Command.js
+++ src/structures/command/Command.js
@@ -10,7 +10,13 @@ module.exports = class Command extends Module {
     this.permissions = options.permissions || []
     this.cooldown = options.cooldown || 5
     this.slash = options.slash || null
+    this.removeDefaultCallback = options.removeDefaultCallback || false
+    this.callback_metadata = {}
   }
+
+  // This is done to return the interaction data so when the user will execute the command automatically the function will be executed.
+  // eslint-disable-next-line no-unused-vars
+  callback(interaction) { }
 
   // eslint-disable-next-line no-unused-vars
   run (ctx) {}
