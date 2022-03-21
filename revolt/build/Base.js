ae31af32cdeff9fe6daaddc1f4d91a5420139856
Nayv <43297329+nayvcake@users.noreply.github.com>
2022-03-21 02:59:24 -0300

    Remove build/

--- revolt/build/Base.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseRevolt = void 0;
-class BaseRevolt {
-    start() { return this; }
-}
-exports.BaseRevolt = BaseRevolt;
