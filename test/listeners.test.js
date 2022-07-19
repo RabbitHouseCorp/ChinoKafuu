f34bbce83c2b9ef557a9f24f636f055a779a1655
nayvcake <43297329+nayvcake@users.noreply.github.com>
2022-07-19 13:37:44 -0300

    Update listeners.test.js

--- test/listeners.test.js
+++ test/listeners.test.js
@@ -1,6 +1,6 @@
 const util = require('./test-utils.js')
 
 test('all listeners can be properly required and initializated', () => {
-  expect(() => util.loadClassesRecursive(`${__dirname}/../discord/src/listeners`))
+  expect(() => util.loadClassesRecursive(`${__dirname}/../apps/discord/src/listeners`))
     .not.toThrow()
 })
