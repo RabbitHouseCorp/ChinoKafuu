d1bb77979733d6ca8c492b3beec9b778c32114ec
Nayv <43297329+nayvcake@users.noreply.github.com>
2023-02-07 15:43:47 -0300

    update: ConstantsTypes

--- apps/discord/src/structures/util/ConstantsTypes.js
+++ apps/discord/src/structures/util/ConstantsTypes.js
@@ -1,9 +1,9 @@
 export const TypeProfession = {
-  busDriver: 0,
-  policie: 1,
-  robber: 2,
-  doctor: 3,
-  lawyer: 4,
-  cabby: 5,
-  domestic: 6,
+  busDriver: [0, 3500],
+  policie: [1, 4175],
+  robber: [2, 1212],
+  doctor: [3, 4414],
+  lawyer: [4, 6675],
+  cabby: [5, 2424],
+  domestic: [6, 1318],
 }
