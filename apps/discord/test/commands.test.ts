8fbf977bf98e21e52df37a192595f489ad2925e4
Nayv <43297329+nayvcake@users.noreply.github.com>
2023-03-13 15:39:34 -0300

    update jest tests
     - Babel is used to compile Typescript tests.
     - Reason for Test Migration from Javascript to Typescript is that there are problems with imports and things native to node that don't work with Module.

--- apps/discord/test/commands.test.ts
+++ apps/discord/test/commands.test.ts
@@ -4,6 +4,28 @@
 /* eslint-disable jest/require-top-level-describe */
 /* eslint-disable jest/prefer-expect-assertions */
 import * as util from './test-utils'
+test('commands', () => {
+  expect(() => {
+    util.getAllFilesRecursive(`${__dirname.replace(/(\\test\/)|(test\/)|(\\test)|(\\test\/)/g, '')}/src/commands`).forEach(c => {
+      const hm = c.getAt('/', -1)
+      if (!hm.endsWith('Command.js') || !hm.isUpperCase(0)) throw new Error(`${c} doesn't following naming rule! Use CommandName (uppercased) *and* the suffix Command.js`)
+      // eslint-disable-next-line security/detect-non-literal-require
+      const resolveImport = require(c)
+      const resolveCommand = resolveImport.default != undefined ? resolveImport.default : resolveImport
+      const Command = new (resolveCommand)()
+
+      if (Command.test !== undefined && typeof Command.test === 'function') {
+        // eslint-disable-next-line no-useless-catch
+        try {
+          // eslint-disable-next-line jest/no-conditional-expect
+          expect(() => Command.test()).toBeDefined()
+        } catch (err) {
+          throw err
+        }
+      }
+    })
+  }).not.toThrow()
+})
 
 test('all commands can be properly required and initializated', () => {
   expect(() => util.loadClassesRecursive(`${__dirname.replace(/(\\test\/)|(test\/)|(\\test)|(\\test\/)/g, '')}/src/commands`))
@@ -16,11 +38,8 @@ test('all commands follow proper naming rules (CommandName and suffix -Command.j
       if (!hm.endsWith('Command.js') || !hm.isUpperCase(0)) throw new Error(`${c} doesn't following naming rule! Use CommandName (uppercased) *and* the suffix Command.js`)
 
       // eslint-disable-next-line security/detect-non-literal-require
-      const resolveImport = require(c)
-      const resolveCommand = resolveImport.default != undefined ? resolveImport.default : resolveImport
-      // const Command = new (resolveCommand)()
-      // hm = hm.getAt('.', 0)
-      // if (hm != Command.constructor.name) throw new Error(`${c}'s class name doesn't match the file name! (file's called ${hm}.js, class's called ${Command.constructor.name})`)
+
     })
   }).not.toThrow()
 })
+
