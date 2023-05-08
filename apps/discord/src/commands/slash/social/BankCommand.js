d97c68a8582385c79add2dc72ec876b5d649eb61
Nayv <43297329+nayvcake@users.noreply.github.com>
2023-05-08 00:57:12 -0300

    new(Command): BankCommand `[BETA]`

---
+++ apps/discord/src/commands/slash/social/BankCommand.js
@@ -0,0 +1,63 @@
+import { CommandBase } from 'eris'
+import { defineState } from '../../../defineTypes/defineState'
+import { Command, SlashCommandContext } from '../../../structures/util'
+
+export default class BankCommand extends Command {
+  constructor() {
+    super({
+      name: 'bank',
+      slash: new CommandBase()
+        .setName('bank')
+        .setDescription('Transfer or check the amount that is in the bank.')
+        .addOptions()
+    })
+  }
+
+  /**
+   * @method run
+   * @param {SlashCommandContext} ctx
+   * @returns {void}
+   */
+  async run(ctx) {
+    const state = defineState({
+      user: ctx.db.user
+    }, {
+      eventEmitter: false
+    })
+    const { economy } = ctx.db.user
+    const text = ctx._locale('commands:bank.message', {
+      0: economy.bank.toLocaleString(),
+      1: economy.value.toLocaleString()
+    })
+
+    ctx.reply('yen', {
+      content: text,
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              style: 1,
+              label: ctx._locale('commands:bank.button.transfer'),
+              custom_id: 'transfer',
+              disabled: false
+            },
+            {
+              type: 2,
+              style: 1,
+              label: ctx._locale('commands:bank.button.withDraw'),
+              custom_id: 'withDraw',
+              disabled: false
+            },
+          ]
+        }
+      ]
+    }).then((message) => {
+      ctx.createInteractionFunction('bankInteraction', message, {
+        state,
+        users: [ctx.message.author.id]
+      })
+    })
+  }
+}
---
+++ apps/discord/src/interactionFunctions/social/BankInteraction.js
@@ -0,0 +1,56 @@
+import { defineInteraction, defineInteractionDefault, defineInteractionFunction } from '../../structures/InteractionFunction'
+import { Logger } from '../../structures/util'
+
+export default defineInteractionDefault(
+  defineInteraction({
+    name: 'bankInteraction',
+  }),
+  defineInteractionFunction(async ({ useModal, editMessageT, _locale, defineState, getData }) => {
+    const { data: dataInteraction } = getData()
+    const type = dataInteraction?.custom_id === 'withDraw' ? ':withDraw' : ':transfer'
+    const component = [{
+      'type': 4,
+      'custom_id': 'value',
+      'label': _locale('commands:bank.interaction.value'),
+      'style': 1,
+      'min_length': 1,
+      'max_length': 9,
+      'placeholder': '',
+      'required': true
+    }]
+
+    useModal(_locale('commands:bank.interaction.title'), async ({ data: interactionData, deleteModal }) => {
+      deleteModal()
+      const { data } = interactionData
+      const [valueComponent] = data.components[0].components
+      const count = parseInt(valueComponent.value.replace(/[^\d-]+/g, ''))
+      let context = 'commands:bank.success.valueWasTransferred'
+
+      if (isNaN(count)) return editMessageT('error', 'commands:bank.error.valueIsInvalid', {})
+      if (count <= 0) return editMessageT('error', 'commands:bank.error.valueNotAcceptable', {})
+
+      const state = defineState.user
+      if (type === ':withDraw') {
+        if (count >= state.economy.bank) return editMessageT('error', 'commands:bank.error.rejectedBankWithdrawalValue', { 0: count.toLocaleString(), 1: state.economy.value.toLocaleString() })
+        context = 'commands:bank.success.valueWasWithdrawnFromBank'
+        state.economy.value += Math.max(count, 0)
+        state.economy.bank -= Math.max(count, 0)
+      } else {
+        if (count >= state.economy.value) return editMessageT('error', 'commands:bank.error.rejectedValue', { 0: count.toLocaleString(), 1: state.economy.value.toLocaleString() })
+        state.economy.value -= Math.max(count, 0)
+        state.economy.bank += Math.max(count, 0)
+      }
+
+      await state.save().catch((err) => {
+        Logger.error(`BankInteractionError: ${err}`)
+        editMessageT('error', 'commands:bank.error.transactionWithError', {})
+        throw err
+      })
+      await editMessageT('success', context, {
+        0: count.toLocaleString(),
+        1: state.economy.bank.toLocaleString(),
+        2: state.economy.value.toLocaleString(),
+      })
+    }, [...component])
+  })
+)
