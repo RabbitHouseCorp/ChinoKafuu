dc90c54c28105491f080787743316b3701607822
Nayv <43297329+nayvcake@users.noreply.github.com>
2023-12-30 16:52:45 -0300

    remove `lavacord` and fix bug

--- apps/discord/src/listeners/voiceChannels/VoiceChannelSwitchListener.js
+++ apps/discord/src/listeners/voiceChannels/VoiceChannelSwitchListener.js
@@ -8,30 +8,16 @@ export default class VoiceChannelLeaveListener extends Listener {
 
   async on(client, member, newChannel, oldChannel) {
     const guild = member.guild
+    if (!client.playerManager.has(guild.id)) return
     const voiceChannel = client.guilds.get(guild.id).channels.get(newChannel.id)
     const server = await client.database.guilds.getOrCreate(guild.id)
-    const guildBot = client.guilds.get(guild.id).members.get(client.user.id)
+    const playerExtend = client.playerManager.getPlayer(guild.id)
     if (server.animu && voiceChannel.id === server.animuChannel) {
-      if (client.player.has(guild.id)) return
-      const song = await client.lavalink.join(voiceChannel.id)
-      song.playAnimu()
-      client.player.set(guild.id, song)
+      if (!playerExtend.player.playingTrack)
+        client.playerManager.getPlayer(guild.id)?.preparePlayer(voiceChannel.id)
     }
 
-    if (newChannel.id !== guildBot.voiceState.channelID) return
-    if (newChannel.voiceMembers.filter(member => member.user.bot === false).length === 0) {
-      await client.lavalink.manager.leave(guild.id)
-      client.lavalink.manager.players.delete(guild.id)
-      client.player.delete(guild.id)
-      return
-    }
-
-    if (oldChannel.id !== guildBot.voiceState.channelID) return
-    if (oldChannel.voiceMembers.filter(member => member.user.bot === false).length === 0) {
-      await client.lavalink.manager.leave(guild.id)
-      client.lavalink.manager.players.delete(guild.id)
-      client.player.delete(guild.id)
-      return
-    }
+    if (playerExtend.player?.voiceInfo?.countUsersConnected === 0)
+      client.playerManager.getPlayer(guild.id)?.delete()
   }
 }
