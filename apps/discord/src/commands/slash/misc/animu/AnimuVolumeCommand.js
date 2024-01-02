6ccfc63c76d7a3e92b2975a0dfb70ca0a8d9ddd1
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2024-01-01 23:50:19 -0300

    Fix a verification in the volume command

--- apps/discord/src/commands/slash/misc/animu/AnimuVolumeCommand.js
+++ apps/discord/src/commands/slash/misc/animu/AnimuVolumeCommand.js
@@ -19,7 +19,7 @@ export default class AnimuVolumeCommand extends Command {
   run(ctx) {
     if (!ctx.client.playerManager.isAvailable) return ctx.replyT('error', 'commands:animu.unavailable')
     if (!ctx.message.guild.members.get(ctx.client.user.id).voiceState.channelID) return ctx.replyT('error', 'baisc:voice.clientAreNotInVoiceChannel')
-    if (!ctx.client.player.has(ctx.message.guild.id)) return ctx.replyT('error', 'basic:voice.playerNotFound')
+    if (!ctx.client.playerManager.getPlayer(ctx.message.guild.id).player.playingTrack) return ctx.replyT('error', 'basic:voice.playerNotFound')
     if (parseInt(ctx.args.get('value').value) > 100) return ctx.replyT('error', 'basic:voice.maxVolume')
     if (parseInt(ctx.args.get('value').value) < 5) return ctx.replyT('error', 'basic:voice.minVolume')
     ctx.client.playerManager.getPlayer(ctx.message.guild.id).setVolume(ctx.args.get('value').value, 100)
